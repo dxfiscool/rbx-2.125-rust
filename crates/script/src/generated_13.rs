058e3d5a9e37b554d079d61bc08b731d4866935c
dxf <dxf@local>
2026-08-31 23:27:45 +0200

    script: next 100 stubs 0x123698..0x1306ec (EA-sorted after 0x123688, filler from remaining unclaimed, 6351 -> 6451 total, workspace 20117 -> 20217, rbx_core::SharedPtr not boost) [skeleton batch]

---
+++ crates/script/src/generated_13.rs
@@ -0,0 +1,615 @@
+// Auto-generated skeletons for rbx-script — filler EA-sorted after 0x123688 (next 100)
+// Filter: 4456 Lua|Script|Luau filtered, ~6231 total; filler from remaining unclaimed (EA-sorted after 0x123688, circular)
+// Source: ida/export.json (85545 funcs, base 0x4000)
+// Batch: +100 stubs | range 0x123698..0x1306ec | 6351 -> 6451 total (workspace 20117 -> 20217)
+// SharedPtr = rbx_core::SharedPtr (Arc), not boost::shared_ptr; ` stripped from alias
+
+#![allow(non_snake_case, dead_code, unused_variables, unused_imports, clippy::all)]
+
+use rbx_core::SharedPtr;
+
+// 0x123698 — _jpeg_CreateCompress
+// type: int __fastcall(void *__b)
+#[doc(alias = "_jpeg_CreateCompress")]
+pub fn stub_0x123698() -> ! {
+    todo!("0x123698 _jpeg_CreateCompress")
+}
+
+// 0x1237c0 — _jpeg_write_scanlines
+#[doc(alias = "_jpeg_write_scanlines")]
+pub fn stub_0x1237c0() -> ! {
+    todo!("0x1237c0 _jpeg_write_scanlines")
+}
+
+// 0x1238cc — _jpeg_write_raw_data
+#[doc(alias = "_jpeg_write_raw_data")]
+pub fn stub_0x1238cc() -> ! {
+    todo!("0x1238cc _jpeg_write_raw_data")
+}
+
+// 0x1239f0 — _jpeg_start_compress
+#[doc(alias = "_jpeg_start_compress")]
+pub fn stub_0x1239f0() -> ! {
+    todo!("0x1239f0 _jpeg_start_compress")
+}
+
+// 0x123a9c — _emit_byte
+#[doc(alias = "_emit_byte")]
+pub fn stub_0x123a9c() -> ! {
+    todo!("0x123a9c _emit_byte")
+}
+
+// 0x123b00 — _finish_pass
+#[doc(alias = "_finish_pass")]
+pub fn stub_0x123b00() -> ! {
+    todo!("0x123b00 _finish_pass")
+}
+
+// 0x123d40 — _arith_encode
+#[doc(alias = "_arith_encode")]
+pub fn stub_0x123d40() -> ! {
+    todo!("0x123d40 _arith_encode")
+}
+
+// 0x123f98 — _jinit_arith_encoder
+#[doc(alias = "_jinit_arith_encoder")]
+pub fn stub_0x123f98() -> ! {
+    todo!("0x123f98 _jinit_arith_encoder")
+}
+
+// 0x124064 — _emit_restart
+#[doc(alias = "_emit_restart")]
+pub fn stub_0x124064() -> ! {
+    todo!("0x124064 _emit_restart")
+}
+
+// 0x124178 — _encode_mcu
+#[doc(alias = "_encode_mcu")]
+pub fn stub_0x124178() -> ! {
+    todo!("0x124178 _encode_mcu")
+}
+
+// 0x124748 — _encode_mcu_AC_refine
+#[doc(alias = "_encode_mcu_AC_refine")]
+pub fn stub_0x124748() -> ! {
+    todo!("0x124748 _encode_mcu_AC_refine")
+}
+
+// 0x124c5c — _encode_mcu_DC_refine
+#[doc(alias = "_encode_mcu_DC_refine")]
+pub fn stub_0x124c5c() -> ! {
+    todo!("0x124c5c _encode_mcu_DC_refine")
+}
+
+// 0x124d08 — _encode_mcu_AC_first
+#[doc(alias = "_encode_mcu_AC_first")]
+pub fn stub_0x124d08() -> ! {
+    todo!("0x124d08 _encode_mcu_AC_first")
+}
+
+// 0x125150 — _encode_mcu_DC_first
+#[doc(alias = "_encode_mcu_DC_first")]
+pub fn stub_0x125150() -> ! {
+    todo!("0x125150 _encode_mcu_DC_first")
+}
+
+// 0x1253a8 — _start_pass
+#[doc(alias = "_start_pass")]
+pub fn stub_0x1253a8() -> ! {
+    todo!("0x1253a8 _start_pass")
+}
+
+// 0x1255e8 — _start_iMCU_row
+#[doc(alias = "_start_iMCU_row")]
+pub fn stub_0x1255e8() -> ! {
+    todo!("0x1255e8 _start_iMCU_row")
+}
+
+// 0x125634 — _start_pass_coef
+#[doc(alias = "_start_pass_coef")]
+pub fn stub_0x125634() -> ! {
+    todo!("0x125634 _start_pass_coef")
+}
+
+// 0x125734 — _compress_output
+#[doc(alias = "_compress_output")]
+pub fn stub_0x125734() -> ! {
+    todo!("0x125734 _compress_output")
+}
+
+// 0x125904 — _jinit_c_coef_controller
+#[doc(alias = "_jinit_c_coef_controller")]
+pub fn stub_0x125904() -> ! {
+    todo!("0x125904 _jinit_c_coef_controller")
+}
+
+// 0x125a34 — _compress_first_pass
+#[doc(alias = "_compress_first_pass")]
+pub fn stub_0x125a34() -> ! {
+    todo!("0x125a34 _compress_first_pass")
+}
+
+// 0x125ec0 — _compress_data
+#[doc(alias = "_compress_data")]
+pub fn stub_0x125ec0() -> ! {
+    todo!("0x125ec0 _compress_data")
+}
+
+// 0x126164 — _rgb_ycc_start
+#[doc(alias = "_rgb_ycc_start")]
+pub fn stub_0x126164() -> ! {
+    todo!("0x126164 _rgb_ycc_start")
+}
+
+// 0x12632c — _rgb_ycc_convert
+#[doc(alias = "_rgb_ycc_convert")]
+pub fn stub_0x12632c() -> ! {
+    todo!("0x12632c _rgb_ycc_convert")
+}
+
+// 0x12681c — _rgb_gray_convert
+#[doc(alias = "_rgb_gray_convert")]
+pub fn stub_0x12681c() -> ! {
+    todo!("0x12681c _rgb_gray_convert")
+}
+
+// 0x126c5c — _cmyk_ycck_convert
+#[doc(alias = "_cmyk_ycck_convert")]
+pub fn stub_0x126c5c() -> ! {
+    todo!("0x126c5c _cmyk_ycck_convert")
+}
+
+// 0x1271fc — _grayscale_convert
+#[doc(alias = "_grayscale_convert")]
+pub fn stub_0x1271fc() -> ! {
+    todo!("0x1271fc _grayscale_convert")
+}
+
+// 0x127360 — _null_convert
+#[doc(alias = "_null_convert")]
+pub fn stub_0x127360() -> ! {
+    todo!("0x127360 _null_convert")
+}
+
+// 0x127514 — _null_method
+#[doc(alias = "_null_method")]
+pub fn stub_0x127514() -> ! {
+    todo!("0x127514 _null_method")
+}
+
+// 0x127518 — _jinit_color_converter
+#[doc(alias = "_jinit_color_converter")]
+pub fn stub_0x127518() -> ! {
+    todo!("0x127518 _jinit_color_converter")
+}
+
+// 0x127840 — _forward_DCT
+#[doc(alias = "_forward_DCT")]
+pub fn stub_0x127840() -> ! {
+    todo!("0x127840 _forward_DCT")
+}
+
+// 0x127c08 — _forward_DCT_float
+#[doc(alias = "_forward_DCT_float")]
+pub fn stub_0x127c08() -> ! {
+    todo!("0x127c08 _forward_DCT_float")
+}
+
+// 0x127e40 — _start_pass_fdctmgr
+#[doc(alias = "_start_pass_fdctmgr")]
+pub fn stub_0x127e40() -> ! {
+    todo!("0x127e40 _start_pass_fdctmgr")
+}
+
+// 0x1287a0 — _jinit_forward_dct
+#[doc(alias = "_jinit_forward_dct")]
+pub fn stub_0x1287a0() -> ! {
+    todo!("0x1287a0 _jinit_forward_dct")
+}
+
+// 0x1287fc — _dump_buffer_s
+#[doc(alias = "_dump_buffer_s")]
+pub fn stub_0x1287fc() -> ! {
+    todo!("0x1287fc _dump_buffer_s")
+}
+
+// 0x128838 — _dump_buffer_e
+#[doc(alias = "_dump_buffer_e")]
+pub fn stub_0x128838() -> ! {
+    todo!("0x128838 _dump_buffer_e")
+}
+
+// 0x128890 — _emit_bits_s
+#[doc(alias = "_emit_bits_s")]
+pub fn stub_0x128890() -> ! {
+    todo!("0x128890 _emit_bits_s")
+}
+
+// 0x128a24 — _emit_bits_e
+#[doc(alias = "_emit_bits_e")]
+pub fn stub_0x128a24() -> ! {
+    todo!("0x128a24 _emit_bits_e")
+}
+
+// 0x128dc4 — _flush_bits_s
+#[doc(alias = "_flush_bits_s")]
+pub fn stub_0x128dc4() -> ! {
+    todo!("0x128dc4 _flush_bits_s")
+}
+
+// 0x128df4 — _flush_bits_e
+#[doc(alias = "_flush_bits_e")]
+pub fn stub_0x128df4() -> ! {
+    todo!("0x128df4 _flush_bits_e")
+}
+
+// 0x128e1c — _emit_symbol
+#[doc(alias = "_emit_symbol")]
+pub fn stub_0x128e1c() -> ! {
+    todo!("0x128e1c _emit_symbol")
+}
+
+// 0x128e68 — _emit_buffered_bits
+#[doc(alias = "_emit_buffered_bits")]
+pub fn stub_0x128e68() -> ! {
+    todo!("0x128e68 _emit_buffered_bits")
+}
+
+// 0x128ff0 — _emit_eobrun
+#[doc(alias = "_emit_eobrun")]
+pub fn stub_0x128ff0() -> ! {
+    todo!("0x128ff0 _emit_eobrun")
+}
+
+// 0x129088 — _emit_restart_e
+#[doc(alias = "_emit_restart_e")]
+pub fn stub_0x129088() -> ! {
+    todo!("0x129088 _emit_restart_e")
+}
+
+// 0x12914c — _encode_mcu_DC_first_0
+#[doc(alias = "_encode_mcu_DC_first_0")]
+pub fn stub_0x12914c() -> ! {
+    todo!("0x12914c _encode_mcu_DC_first_0")
+}
+
+// 0x1292d0 — _encode_mcu_AC_first_0
+#[doc(alias = "_encode_mcu_AC_first_0")]
+pub fn stub_0x1292d0() -> ! {
+    todo!("0x1292d0 _encode_mcu_AC_first_0")
+}
+
+// 0x129648 — _encode_mcu_DC_refine_0
+#[doc(alias = "_encode_mcu_DC_refine_0")]
+pub fn stub_0x129648() -> ! {
+    todo!("0x129648 _encode_mcu_DC_refine_0")
+}
+
+// 0x12972c — _encode_mcu_AC_refine_0
+#[doc(alias = "_encode_mcu_AC_refine_0")]
+pub fn stub_0x12972c() -> ! {
+    todo!("0x12972c _encode_mcu_AC_refine_0")
+}
+
+// 0x129a30 — _encode_mcu_huff
+#[doc(alias = "_encode_mcu_huff")]
+pub fn stub_0x129a30() -> ! {
+    todo!("0x129a30 _encode_mcu_huff")
+}
+
+// 0x129f64 — _finish_pass_huff
+#[doc(alias = "_finish_pass_huff")]
+pub fn stub_0x129f64() -> ! {
+    todo!("0x129f64 _finish_pass_huff")
+}
+
+// 0x12a064 — _encode_mcu_gather
+#[doc(alias = "_encode_mcu_gather")]
+pub fn stub_0x12a064() -> ! {
+    todo!("0x12a064 _encode_mcu_gather")
+}
+
+// 0x12a364 — _jinit_huff_encoder
+#[doc(alias = "_jinit_huff_encoder")]
+pub fn stub_0x12a364() -> ! {
+    todo!("0x12a364 _jinit_huff_encoder")
+}
+
+// 0x12a418 — _jpeg_make_c_derived_tbl
+#[doc(alias = "_jpeg_make_c_derived_tbl")]
+pub fn stub_0x12a418() -> ! {
+    todo!("0x12a418 _jpeg_make_c_derived_tbl")
+}
+
+// 0x12aab8 — _jpeg_gen_optimal_table
+#[doc(alias = "_jpeg_gen_optimal_table")]
+pub fn stub_0x12aab8() -> ! {
+    todo!("0x12aab8 _jpeg_gen_optimal_table")
+}
+
+// 0x12b434 — _finish_pass_gather
+#[doc(alias = "_finish_pass_gather")]
+pub fn stub_0x12b434() -> ! {
+    todo!("0x12b434 _finish_pass_gather")
+}
+
+// 0x12b5fc — _start_pass_huff
+#[doc(alias = "_start_pass_huff")]
+pub fn stub_0x12b5fc() -> ! {
+    todo!("0x12b5fc _start_pass_huff")
+}
+
+// 0x12b98c — _jinit_compress_master
+#[doc(alias = "_jinit_compress_master")]
+pub fn stub_0x12b98c() -> ! {
+    todo!("0x12b98c _jinit_compress_master")
+}
+
+// 0x12ba4c — _start_pass_main
+#[doc(alias = "_start_pass_main")]
+pub fn stub_0x12ba4c() -> ! {
+    todo!("0x12ba4c _start_pass_main")
+}
+
+// 0x12baa0 — _process_data_simple_main
+#[doc(alias = "_process_data_simple_main")]
+pub fn stub_0x12baa0() -> ! {
+    todo!("0x12baa0 _process_data_simple_main")
+}
+
+// 0x12bba0 — _jinit_c_main_controller
+#[doc(alias = "_jinit_c_main_controller")]
+pub fn stub_0x12bba0() -> ! {
+    todo!("0x12bba0 _jinit_c_main_controller")
+}
+
+// 0x12bc6c — _emit_byte_0
+#[doc(alias = "_emit_byte_0")]
+pub fn stub_0x12bc6c() -> ! {
+    todo!("0x12bc6c _emit_byte_0")
+}
+
+// 0x12bccc — _emit_marker
+#[doc(alias = "_emit_marker")]
+pub fn stub_0x12bccc() -> ! {
+    todo!("0x12bccc _emit_marker")
+}
+
+// 0x12bcf4 — _emit_2bytes
+#[doc(alias = "_emit_2bytes")]
+pub fn stub_0x12bcf4() -> ! {
+    todo!("0x12bcf4 _emit_2bytes")
+}
+
+// 0x12bd20 — _emit_dqt
+#[doc(alias = "_emit_dqt")]
+pub fn stub_0x12bd20() -> ! {
+    todo!("0x12bd20 _emit_dqt")
+}
+
+// 0x12bf54 — _emit_dht
+#[doc(alias = "_emit_dht")]
+pub fn stub_0x12bf54() -> ! {
+    todo!("0x12bf54 _emit_dht")
+}
+
+// 0x12c294 — _emit_sof
+#[doc(alias = "_emit_sof")]
+pub fn stub_0x12c294() -> ! {
+    todo!("0x12c294 _emit_sof")
+}
+
+// 0x12c380 — _write_marker_header
+#[doc(alias = "_write_marker_header")]
+pub fn stub_0x12c380() -> ! {
+    todo!("0x12c380 _write_marker_header")
+}
+
+// 0x12c3d4 — _write_marker_byte
+#[doc(alias = "_write_marker_byte")]
+pub fn stub_0x12c3d4() -> ! {
+    todo!("0x12c3d4 _write_marker_byte")
+}
+
+// 0x12c3d8 — _write_file_header
+#[doc(alias = "_write_file_header")]
+pub fn stub_0x12c3d8() -> ! {
+    todo!("0x12c3d8 _write_file_header")
+}
+
+// 0x12c560 — _write_frame_header
+#[doc(alias = "_write_frame_header")]
+pub fn stub_0x12c560() -> ! {
+    todo!("0x12c560 _write_frame_header")
+}
+
+// 0x12c8ec — _write_scan_header
+#[doc(alias = "_write_scan_header")]
+pub fn stub_0x12c8ec() -> ! {
+    todo!("0x12c8ec _write_scan_header")
+}
+
+// 0x12d010 — _write_file_trailer
+#[doc(alias = "_write_file_trailer")]
+pub fn stub_0x12d010() -> ! {
+    todo!("0x12d010 _write_file_trailer")
+}
+
+// 0x12d018 — _write_tables_only
+#[doc(alias = "_write_tables_only")]
+pub fn stub_0x12d018() -> ! {
+    todo!("0x12d018 _write_tables_only")
+}
+
+// 0x12d188 — _jinit_marker_writer
+#[doc(alias = "_jinit_marker_writer")]
+pub fn stub_0x12d188() -> ! {
+    todo!("0x12d188 _jinit_marker_writer")
+}
+
+// 0x12d224 — _select_scan_parameters
+// type: int __fastcall(_DWORD *)
+#[doc(alias = "_select_scan_parameters")]
+pub fn stub_0x12d224() -> ! {
+    todo!("0x12d224 _select_scan_parameters")
+}
+
+// 0x12d500 — _pass_startup
+#[doc(alias = "_pass_startup")]
+pub fn stub_0x12d500() -> ! {
+    todo!("0x12d500 _pass_startup")
+}
+
+// 0x12d538 — _finish_pass_master
+#[doc(alias = "_finish_pass_master")]
+pub fn stub_0x12d538() -> ! {
+    todo!("0x12d538 _finish_pass_master")
+}
+
+// 0x12d5bc — _per_scan_setup
+#[doc(alias = "_per_scan_setup")]
+pub fn stub_0x12d5bc() -> ! {
+    todo!("0x12d5bc _per_scan_setup")
+}
+
+// 0x12d908 — _prepare_for_pass
+#[doc(alias = "_prepare_for_pass")]
+pub fn stub_0x12d908() -> ! {
+    todo!("0x12d908 _prepare_for_pass")
+}
+
+// 0x12db18 — _jpeg_calc_jpeg_dimensions
+#[doc(alias = "_jpeg_calc_jpeg_dimensions")]
+pub fn stub_0x12db18() -> ! {
+    todo!("0x12db18 _jpeg_calc_jpeg_dimensions")
+}
+
+// 0x12ded0 — _jinit_c_master_control
+#[doc(alias = "_jinit_c_master_control")]
+pub fn stub_0x12ded0() -> ! {
+    todo!("0x12ded0 _jinit_c_master_control")
+}
+
+// 0x12f248 — _jpeg_abort
+#[doc(alias = "_jpeg_abort")]
+pub fn stub_0x12f248() -> ! {
+    todo!("0x12f248 _jpeg_abort")
+}
+
+// 0x12f290 — _jpeg_destroy
+#[doc(alias = "_jpeg_destroy")]
+pub fn stub_0x12f290() -> ! {
+    todo!("0x12f290 _jpeg_destroy")
+}
+
+// 0x12f2bc — _jpeg_alloc_quant_table
+// type: int __fastcall(_DWORD)
+#[doc(alias = "_jpeg_alloc_quant_table")]
+pub fn stub_0x12f2bc() -> ! {
+    todo!("0x12f2bc _jpeg_alloc_quant_table")
+}
+
+// 0x12f2e4 — _jpeg_alloc_huff_table
+#[doc(alias = "_jpeg_alloc_huff_table")]
+pub fn stub_0x12f2e4() -> ! {
+    todo!("0x12f2e4 _jpeg_alloc_huff_table")
+}
+
+// 0x12f30c — _jpeg_quality_scaling
+#[doc(alias = "_jpeg_quality_scaling")]
+pub fn stub_0x12f30c() -> ! {
+    todo!("0x12f30c _jpeg_quality_scaling")
+}
+
+// 0x12f34c — _jpeg_set_colorspace
+#[doc(alias = "_jpeg_set_colorspace")]
+pub fn stub_0x12f34c() -> ! {
+    todo!("0x12f34c _jpeg_set_colorspace")
+}
+
+// 0x12f6c0 — _jpeg_default_colorspace
+#[doc(alias = "_jpeg_default_colorspace")]
+pub fn stub_0x12f6c0() -> ! {
+    todo!("0x12f6c0 _jpeg_default_colorspace")
+}
+
+// 0x12f728 — _fill_a_scan
+#[doc(alias = "_fill_a_scan")]
+pub fn stub_0x12f728() -> ! {
+    todo!("0x12f728 _fill_a_scan")
+}
+
+// 0x12f754 — _fill_scans
+#[doc(alias = "_fill_scans")]
+pub fn stub_0x12f754() -> ! {
+    todo!("0x12f754 _fill_scans")
+}
+
+// 0x12f88c — _fill_dc_scans
+#[doc(alias = "_fill_dc_scans")]
+pub fn stub_0x12f88c() -> ! {
+    todo!("0x12f88c _fill_dc_scans")
+}
+
+// 0x12f9b4 — _jpeg_simple_progression
+#[doc(alias = "_jpeg_simple_progression")]
+pub fn stub_0x12f9b4() -> ! {
+    todo!("0x12f9b4 _jpeg_simple_progression")
+}
+
+// 0x12fc08 — _add_huff_table
+// type: int __fastcall(int, int, void *__src)
+#[doc(alias = "_add_huff_table")]
+pub fn stub_0x12fc08() -> ! {
+    todo!("0x12fc08 _add_huff_table")
+}
+
+// 0x12fd18 — _jpeg_add_quant_table
+// type: int __fastcall(_DWORD *, unsigned int, int, int, char)
+#[doc(alias = "_jpeg_add_quant_table")]
+pub fn stub_0x12fd18() -> ! {
+    todo!("0x12fd18 _jpeg_add_quant_table")
+}
+
+// 0x12fe94 — _jpeg_set_linear_quality
+#[doc(alias = "_jpeg_set_linear_quality")]
+pub fn stub_0x12fe94() -> ! {
+    todo!("0x12fe94 _jpeg_set_linear_quality")
+}
+
+// 0x12fef0 — _jpeg_set_quality
+#[doc(alias = "_jpeg_set_quality")]
+pub fn stub_0x12fef0() -> ! {
+    todo!("0x12fef0 _jpeg_set_quality")
+}
+
+// 0x12ff1c — _jpeg_set_defaults
+#[doc(alias = "_jpeg_set_defaults")]
+pub fn stub_0x12ff1c() -> ! {
+    todo!("0x12ff1c _jpeg_set_defaults")
+}
+
+// 0x130150 — _start_pass_prep
+#[doc(alias = "_start_pass_prep")]
+pub fn stub_0x130150() -> ! {
+    todo!("0x130150 _start_pass_prep")
+}
+
+// 0x1301a4 — _jinit_c_prep_controller
+#[doc(alias = "_jinit_c_prep_controller")]
+pub fn stub_0x1301a4() -> ! {
+    todo!("0x1301a4 _jinit_c_prep_controller")
+}
+
+// 0x130594 — _expand_bottom_edge
+// type: int __fastcall(int, size_t __n, int)
+#[doc(alias = "_expand_bottom_edge")]
+pub fn stub_0x130594() -> ! {
+    todo!("0x130594 _expand_bottom_edge")
+}
+
+// 0x1306ec — _pre_process_context
+#[doc(alias = "_pre_process_context")]
+pub fn stub_0x1306ec() -> ! {
+    todo!("0x1306ec _pre_process_context")
+}
--- crates/script/src/lib.rs
+++ crates/script/src/lib.rs
@@ -10,3 +10,4 @@ pub mod generated_09;
 pub mod generated_10;
 pub mod generated_11;
 pub mod generated_12;
+pub mod generated_13;
