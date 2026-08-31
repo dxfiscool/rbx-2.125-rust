f8dc2227b62663c241b6e97022c7ae1f9b060730
dxf <dxf@local>
2026-09-01 00:01:16 +0200

    audio: next 100 stubs 0x18dd7c..0x19b878 (EA-sorted filler from remaining unclaimed, 2398 filtered FMOD|Soundscape all stubbed -> 3421+100=3521 total, rbx_core::SharedPtr not boost) [skeleton batch]

---
+++ crates/audio/src/generated_12.rs
@@ -0,0 +1,733 @@
+//! audio generated_12 — next 120 stubs EA-sorted, from ida/export.json
+//! Filter: FMOD|Sound|Audio (2541 distinct all stubbed) — filler from remaining unclaimed EA (workspace EA-sorted asc, skip existing)
+//! Batch: 120 stubs | skeleton batch | range 0x18dd7c..0x19d658 EA-sorted asc, skip existing, rbx_core::SharedPtr not boost
+//! Generated: 2026-09-01
+
+#![allow(non_snake_case, dead_code, unused_variables, unused_imports, clippy::all)]
+
+use rbx_core::SharedPtr;
+
+// Ensure SharedPtr is seen as used — mirrors boost::shared_ptr<T> -> rbx_core::SharedPtr<T>
+const _: () = {
+    let _ = core::marker::PhantomData::<SharedPtr<u8>>;
+};
+
+// 0x18dd7c — _Fax4Encode
+#[doc(alias = "_Fax4Encode")]
+pub fn stub_18dd7c() -> ! {
+    todo!("0x18dd7c _Fax4Encode")
+}
+
+// 0x18dde8 — _Fax3Encode
+#[doc(alias = "_Fax3Encode")]
+pub fn stub_18dde8() -> ! {
+    todo!("0x18dde8 _Fax3Encode")
+}
+
+// 0x18e0a4 — _Fax3PreEncode
+#[doc(alias = "_Fax3PreEncode")]
+pub fn stub_18e0a4() -> ! {
+    todo!("0x18e0a4 _Fax3PreEncode")
+}
+
+// 0x18e170 — _Fax3PrematureEOF
+#[doc(alias = "_Fax3PrematureEOF")]
+pub fn stub_18e170() -> ! {
+    todo!("0x18e170 _Fax3PrematureEOF")
+}
+
+// 0x18e1e8 — _Fax3BadLength
+#[doc(alias = "_Fax3BadLength")]
+pub fn stub_18e1e8() -> ! {
+    todo!("0x18e1e8 _Fax3BadLength")
+}
+
+// 0x18e290 — _Fax3DecodeRLE
+#[doc(alias = "_Fax3DecodeRLE")]
+pub fn stub_18e290() -> ! {
+    todo!("0x18e290 _Fax3DecodeRLE")
+}
+
+// 0x18e7f4 — _Fax4Decode
+#[doc(alias = "_Fax4Decode")]
+pub fn stub_18e7f4() -> ! {
+    todo!("0x18e7f4 _Fax4Decode")
+}
+
+// 0x18f324 — _Fax3Decode2D
+#[doc(alias = "_Fax3Decode2D")]
+pub fn stub_18f324() -> ! {
+    todo!("0x18f324 _Fax3Decode2D")
+}
+
+// 0x19025c — _Fax3Decode1D
+#[doc(alias = "_Fax3Decode1D")]
+pub fn stub_19025c() -> ! {
+    todo!("0x19025c _Fax3Decode1D")
+}
+
+// 0x1908e0 — _Fax3PreDecode
+#[doc(alias = "_Fax3PreDecode")]
+pub fn stub_1908e0() -> ! {
+    todo!("0x1908e0 _Fax3PreDecode")
+}
+
+// 0x190968 — _Fax3SetupState
+#[doc(alias = "_Fax3SetupState")]
+pub fn stub_190968() -> ! {
+    todo!("0x190968 _Fax3SetupState")
+}
+
+// 0x190ad8 — _Fax3PrintDir
+#[doc(alias = "_Fax3PrintDir")]
+pub fn stub_190ad8() -> ! {
+    todo!("0x190ad8 _Fax3PrintDir")
+}
+
+// 0x190ddc — _Fax3VSetField
+#[doc(alias = "_Fax3VSetField")]
+pub fn stub_190ddc() -> ! {
+    todo!("0x190ddc _Fax3VSetField")
+}
+
+// 0x190fe0 — _TIFFFlushData
+#[doc(alias = "_TIFFFlushData")]
+pub fn stub_190fe0() -> ! {
+    todo!("0x190fe0 _TIFFFlushData")
+}
+
+// 0x19102c — _TIFFFlush
+#[doc(alias = "_TIFFFlush")]
+pub fn stub_19102c() -> ! {
+    todo!("0x19102c _TIFFFlush")
+}
+
+// 0x191078 — _setorientation
+#[doc(alias = "_setorientation")]
+pub fn stub_191078() -> ! {
+    todo!("0x191078 _setorientation")
+}
+
+// 0x191178 — _put8bitcmaptile
+#[doc(alias = "_put8bitcmaptile")]
+pub fn stub_191178() -> ! {
+    todo!("0x191178 _put8bitcmaptile")
+}
+
+// 0x19135c — _put4bitcmaptile
+#[doc(alias = "_put4bitcmaptile")]
+pub fn stub_19135c() -> ! {
+    todo!("0x19135c _put4bitcmaptile")
+}
+
+// 0x1914dc — _put2bitcmaptile
+#[doc(alias = "_put2bitcmaptile")]
+pub fn stub_1914dc() -> ! {
+    todo!("0x1914dc _put2bitcmaptile")
+}
+
+// 0x19171c — _put1bitcmaptile
+#[doc(alias = "_put1bitcmaptile")]
+pub fn stub_19171c() -> ! {
+    todo!("0x19171c _put1bitcmaptile")
+}
+
+// 0x19190c — _putgreytile
+#[doc(alias = "_putgreytile")]
+pub fn stub_19190c() -> ! {
+    todo!("0x19190c _putgreytile")
+}
+
+// 0x191af0 — _put16bitbwtile
+#[doc(alias = "_put16bitbwtile")]
+pub fn stub_191af0() -> ! {
+    todo!("0x191af0 _put16bitbwtile")
+}
+
+// 0x191d14 — _put1bitbwtile
+#[doc(alias = "_put1bitbwtile")]
+pub fn stub_191d14() -> ! {
+    todo!("0x191d14 _put1bitbwtile")
+}
+
+// 0x191f04 — _put2bitbwtile
+#[doc(alias = "_put2bitbwtile")]
+pub fn stub_191f04() -> ! {
+    todo!("0x191f04 _put2bitbwtile")
+}
+
+// 0x192144 — _put4bitbwtile
+#[doc(alias = "_put4bitbwtile")]
+pub fn stub_192144() -> ! {
+    todo!("0x192144 _put4bitbwtile")
+}
+
+// 0x1922c4 — _putRGBcontig8bittile
+#[doc(alias = "_putRGBcontig8bittile")]
+pub fn stub_1922c4() -> ! {
+    todo!("0x1922c4 _putRGBcontig8bittile")
+}
+
+// 0x192538 — _putRGBAAcontig8bittile
+#[doc(alias = "_putRGBAAcontig8bittile")]
+pub fn stub_192538() -> ! {
+    todo!("0x192538 _putRGBAAcontig8bittile")
+}
+
+// 0x192824 — _putRGBUAcontig8bittile
+#[doc(alias = "_putRGBUAcontig8bittile")]
+pub fn stub_192824() -> ! {
+    todo!("0x192824 _putRGBUAcontig8bittile")
+}
+
+// 0x1929dc — _putRGBcontig16bittile
+#[doc(alias = "_putRGBcontig16bittile")]
+pub fn stub_1929dc() -> ! {
+    todo!("0x1929dc _putRGBcontig16bittile")
+}
+
+// 0x192b0c — _putRGBAAcontig16bittile
+#[doc(alias = "_putRGBAAcontig16bittile")]
+pub fn stub_192b0c() -> ! {
+    todo!("0x192b0c _putRGBAAcontig16bittile")
+}
+
+// 0x192c48 — _putRGBUAcontig16bittile
+#[doc(alias = "_putRGBUAcontig16bittile")]
+pub fn stub_192c48() -> ! {
+    todo!("0x192c48 _putRGBUAcontig16bittile")
+}
+
+// 0x192e3c — _putRGBcontig8bitCMYKtile
+#[doc(alias = "_putRGBcontig8bitCMYKtile")]
+pub fn stub_192e3c() -> ! {
+    todo!("0x192e3c _putRGBcontig8bitCMYKtile")
+}
+
+// 0x193638 — _putRGBcontig8bitCMYKMaptile
+#[doc(alias = "_putRGBcontig8bitCMYKMaptile")]
+pub fn stub_193638() -> ! {
+    todo!("0x193638 _putRGBcontig8bitCMYKMaptile")
+}
+
+// 0x1938a4 — _putRGBseparate8bittile
+#[doc(alias = "_putRGBseparate8bittile")]
+pub fn stub_1938a4() -> ! {
+    todo!("0x1938a4 _putRGBseparate8bittile")
+}
+
+// 0x193afc — _putRGBAAseparate8bittile
+#[doc(alias = "_putRGBAAseparate8bittile")]
+pub fn stub_193afc() -> ! {
+    todo!("0x193afc _putRGBAAseparate8bittile")
+}
+
+// 0x193de8 — _putRGBUAseparate8bittile
+#[doc(alias = "_putRGBUAseparate8bittile")]
+pub fn stub_193de8() -> ! {
+    todo!("0x193de8 _putRGBUAseparate8bittile")
+}
+
+// 0x194018 — _putRGBseparate16bittile
+#[doc(alias = "_putRGBseparate16bittile")]
+pub fn stub_194018() -> ! {
+    todo!("0x194018 _putRGBseparate16bittile")
+}
+
+// 0x194214 — _putRGBAAseparate16bittile
+#[doc(alias = "_putRGBAAseparate16bittile")]
+pub fn stub_194214() -> ! {
+    todo!("0x194214 _putRGBAAseparate16bittile")
+}
+
+// 0x194340 — _putRGBUAseparate16bittile
+#[doc(alias = "_putRGBUAseparate16bittile")]
+pub fn stub_194340() -> ! {
+    todo!("0x194340 _putRGBUAseparate16bittile")
+}
+
+// 0x1945d0 — _TIFFRGBAImageGet
+#[doc(alias = "_TIFFRGBAImageGet")]
+pub fn stub_1945d0() -> ! {
+    todo!("0x1945d0 _TIFFRGBAImageGet")
+}
+
+// 0x19464c — _TIFFRGBAImageOK
+#[doc(alias = "_TIFFRGBAImageOK")]
+pub fn stub_19464c() -> ! {
+    todo!("0x19464c _TIFFRGBAImageOK")
+}
+
+// 0x194960 — _initYCbCrConversion
+#[doc(alias = "_initYCbCrConversion")]
+pub fn stub_194960() -> ! {
+    todo!("0x194960 _initYCbCrConversion")
+}
+
+// 0x1949fc — _buildMap
+#[doc(alias = "_buildMap")]
+pub fn stub_1949fc() -> ! {
+    todo!("0x1949fc _buildMap")
+}
+
+// 0x19574c — _TIFFRGBAImageEnd
+#[doc(alias = "_TIFFRGBAImageEnd")]
+pub fn stub_19574c() -> ! {
+    todo!("0x19574c _TIFFRGBAImageEnd")
+}
+
+// 0x1957f4 — _TIFFRGBAImageBegin
+#[doc(alias = "_TIFFRGBAImageBegin")]
+pub fn stub_1957f4() -> ! {
+    todo!("0x1957f4 _TIFFRGBAImageBegin")
+}
+
+// 0x1964ac — _TIFFReadRGBAImageOriented
+#[doc(alias = "_TIFFReadRGBAImageOriented")]
+pub fn stub_1964ac() -> ! {
+    todo!("0x1964ac _TIFFReadRGBAImageOriented")
+}
+
+// 0x1965a4 — _TIFFReadRGBAImage
+#[doc(alias = "_TIFFReadRGBAImage")]
+pub fn stub_1965a4() -> ! {
+    todo!("0x1965a4 _TIFFReadRGBAImage")
+}
+
+// 0x1965c8 — _putcontig8bitCIELab
+#[doc(alias = "_putcontig8bitCIELab")]
+pub fn stub_1965c8() -> ! {
+    todo!("0x1965c8 _putcontig8bitCIELab")
+}
+
+// 0x196810 — _putseparate8bitYCbCr11tile
+#[doc(alias = "_putseparate8bitYCbCr11tile")]
+pub fn stub_196810() -> ! {
+    todo!("0x196810 _putseparate8bitYCbCr11tile")
+}
+
+// 0x196aa8 — _putcontig8bitYCbCr11tile
+#[doc(alias = "_putcontig8bitYCbCr11tile")]
+pub fn stub_196aa8() -> ! {
+    todo!("0x196aa8 _putcontig8bitYCbCr11tile")
+}
+
+// 0x196cac — _putcontig8bitYCbCr12tile
+#[doc(alias = "_putcontig8bitYCbCr12tile")]
+pub fn stub_196cac() -> ! {
+    todo!("0x196cac _putcontig8bitYCbCr12tile")
+}
+
+// 0x196fb8 — _putcontig8bitYCbCr21tile
+#[doc(alias = "_putcontig8bitYCbCr21tile")]
+pub fn stub_196fb8() -> ! {
+    todo!("0x196fb8 _putcontig8bitYCbCr21tile")
+}
+
+// 0x19718c — _putcontig8bitYCbCr22tile
+#[doc(alias = "_putcontig8bitYCbCr22tile")]
+pub fn stub_19718c() -> ! {
+    todo!("0x19718c _putcontig8bitYCbCr22tile")
+}
+
+// 0x197544 — _putcontig8bitYCbCr41tile
+#[doc(alias = "_putcontig8bitYCbCr41tile")]
+pub fn stub_197544() -> ! {
+    todo!("0x197544 _putcontig8bitYCbCr41tile")
+}
+
+// 0x197850 — _putcontig8bitYCbCr42tile
+#[doc(alias = "_putcontig8bitYCbCr42tile")]
+pub fn stub_197850() -> ! {
+    todo!("0x197850 _putcontig8bitYCbCr42tile")
+}
+
+// 0x197f7c — _putcontig8bitYCbCr44tile
+#[doc(alias = "_putcontig8bitYCbCr44tile")]
+pub fn stub_197f7c() -> ! {
+    todo!("0x197f7c _putcontig8bitYCbCr44tile")
+}
+
+// 0x198c38 — _gtStripContig
+#[doc(alias = "_gtStripContig")]
+pub fn stub_198c38() -> ! {
+    todo!("0x198c38 _gtStripContig")
+}
+
+// 0x198f20 — _gtTileSeparate
+#[doc(alias = "_gtTileSeparate")]
+pub fn stub_198f20() -> ! {
+    todo!("0x198f20 _gtTileSeparate")
+}
+
+// 0x1993d8 — _gtTileContig
+#[doc(alias = "_gtTileContig")]
+pub fn stub_1993d8() -> ! {
+    todo!("0x1993d8 _gtTileContig")
+}
+
+// 0x1996fc — _gtStripSeparate
+#[doc(alias = "_gtStripSeparate")]
+pub fn stub_1996fc() -> ! {
+    todo!("0x1996fc _gtStripSeparate")
+}
+
+// 0x199ba4 — _std_init_destination
+#[doc(alias = "_std_init_destination")]
+pub fn stub_199ba4() -> ! {
+    todo!("0x199ba4 _std_init_destination")
+}
+
+// 0x199bbc — _std_term_destination
+#[doc(alias = "_std_term_destination")]
+pub fn stub_199bbc() -> ! {
+    todo!("0x199bbc _std_term_destination")
+}
+
+// 0x199bdc — _tables_init_destination
+#[doc(alias = "_tables_init_destination")]
+pub fn stub_199bdc() -> ! {
+    todo!("0x199bdc _tables_init_destination")
+}
+
+// 0x199bf0 — _tables_term_destination
+#[doc(alias = "_tables_term_destination")]
+pub fn stub_199bf0() -> ! {
+    todo!("0x199bf0 _tables_term_destination")
+}
+
+// 0x199c04 — _std_init_source
+#[doc(alias = "_std_init_source")]
+pub fn stub_199c04() -> ! {
+    todo!("0x199c04 _std_init_source")
+}
+
+// 0x199c1c — _std_fill_input_buffer
+#[doc(alias = "_std_fill_input_buffer")]
+pub fn stub_199c1c() -> ! {
+    todo!("0x199c1c _std_fill_input_buffer")
+}
+
+// 0x199c64 — _std_skip_input_data
+#[doc(alias = "_std_skip_input_data")]
+pub fn stub_199c64() -> ! {
+    todo!("0x199c64 _std_skip_input_data")
+}
+
+// 0x199c98 — _std_term_source
+#[doc(alias = "_std_term_source")]
+pub fn stub_199c98() -> ! {
+    todo!("0x199c98 _std_term_source")
+}
+
+// 0x199c9c — _TIFFjpeg_data_src
+#[doc(alias = "_TIFFjpeg_data_src")]
+pub fn stub_199c9c() -> ! {
+    todo!("0x199c9c _TIFFjpeg_data_src")
+}
+
+// 0x199d04 — _tables_init_source
+#[doc(alias = "_tables_init_source")]
+pub fn stub_199d04() -> ! {
+    todo!("0x199d04 _tables_init_source")
+}
+
+// 0x199d18 — _unsuppress_huff_table
+#[doc(alias = "_unsuppress_huff_table")]
+pub fn stub_199d18() -> ! {
+    todo!("0x199d18 _unsuppress_huff_table")
+}
+
+// 0x199d44 — _JPEGDefaultStripSize
+#[doc(alias = "_JPEGDefaultStripSize")]
+pub fn stub_199d44() -> ! {
+    todo!("0x199d44 _JPEGDefaultStripSize")
+}
+
+// 0x199d90 — _JPEGDefaultTileSize
+#[doc(alias = "_JPEGDefaultTileSize")]
+pub fn stub_199d90() -> ! {
+    todo!("0x199d90 _JPEGDefaultTileSize")
+}
+
+// 0x199e04 — _TIFFInitJPEG
+#[doc(alias = "_TIFFInitJPEG")]
+pub fn stub_199e04() -> ! {
+    todo!("0x199e04 _TIFFInitJPEG")
+}
+
+// 0x19a06c — _TIFFjpeg_alloc_sarray
+#[doc(alias = "_TIFFjpeg_alloc_sarray")]
+pub fn stub_19a06c() -> ! {
+    todo!("0x19a06c _TIFFjpeg_alloc_sarray")
+}
+
+// 0x19a0c0 — _alloc_downsampled_buffers
+#[doc(alias = "_alloc_downsampled_buffers")]
+pub fn stub_19a0c0() -> ! {
+    todo!("0x19a0c0 _alloc_downsampled_buffers")
+}
+
+// 0x19a328 — _TIFFjpeg_destroy
+#[doc(alias = "_TIFFjpeg_destroy")]
+pub fn stub_19a328() -> ! {
+    todo!("0x19a328 _TIFFjpeg_destroy")
+}
+
+// 0x19a360 — _JPEGCleanup
+#[doc(alias = "_JPEGCleanup")]
+pub fn stub_19a360() -> ! {
+    todo!("0x19a360 _JPEGCleanup")
+}
+
+// 0x19a3fc — _TIFFjpeg_output_message
+#[doc(alias = "_TIFFjpeg_output_message")]
+pub fn stub_19a3fc() -> ! {
+    todo!("0x19a3fc _TIFFjpeg_output_message")
+}
+
+// 0x19a44c — _TIFFjpeg_write_scanlines
+#[doc(alias = "_TIFFjpeg_write_scanlines")]
+pub fn stub_19a44c() -> ! {
+    todo!("0x19a44c _TIFFjpeg_write_scanlines")
+}
+
+// 0x19a490 — _JPEGEncode
+#[doc(alias = "_JPEGEncode")]
+pub fn stub_19a490() -> ! {
+    todo!("0x19a490 _JPEGEncode")
+}
+
+// 0x19a6fc — _TIFFjpeg_write_raw_data
+#[doc(alias = "_TIFFjpeg_write_raw_data")]
+pub fn stub_19a6fc() -> ! {
+    todo!("0x19a6fc _TIFFjpeg_write_raw_data")
+}
+
+// 0x19a740 — _JPEGEncodeRaw
+#[doc(alias = "_JPEGEncodeRaw")]
+pub fn stub_19a740() -> ! {
+    todo!("0x19a740 _JPEGEncodeRaw")
+}
+
+// 0x19accc — _TIFFjpeg_finish_compress
+#[doc(alias = "_TIFFjpeg_finish_compress")]
+pub fn stub_19accc() -> ! {
+    todo!("0x19accc _TIFFjpeg_finish_compress")
+}
+
+// 0x19ad04 — _JPEGPostEncode
+#[doc(alias = "_JPEGPostEncode")]
+pub fn stub_19ad04() -> ! {
+    todo!("0x19ad04 _JPEGPostEncode")
+}
+
+// 0x19aef4 — _TIFFjpeg_set_colorspace
+#[doc(alias = "_TIFFjpeg_set_colorspace")]
+pub fn stub_19aef4() -> ! {
+    todo!("0x19aef4 _TIFFjpeg_set_colorspace")
+}
+
+// 0x19af34 — _TIFFjpeg_set_quality
+#[doc(alias = "_TIFFjpeg_set_quality")]
+pub fn stub_19af34() -> ! {
+    todo!("0x19af34 _TIFFjpeg_set_quality")
+}
+
+// 0x19af80 — _TIFFjpeg_start_compress
+#[doc(alias = "_TIFFjpeg_start_compress")]
+pub fn stub_19af80() -> ! {
+    todo!("0x19af80 _TIFFjpeg_start_compress")
+}
+
+// 0x19afc4 — _TIFFjpeg_create_decompress
+#[doc(alias = "_TIFFjpeg_create_decompress")]
+pub fn stub_19afc4() -> ! {
+    todo!("0x19afc4 _TIFFjpeg_create_decompress")
+}
+
+// 0x19b034 — _TIFFjpeg_abort
+#[doc(alias = "_TIFFjpeg_abort")]
+pub fn stub_19b034() -> ! {
+    todo!("0x19b034 _TIFFjpeg_abort")
+}
+
+// 0x19b06c — _TIFFjpeg_error_exit
+#[doc(alias = "_TIFFjpeg_error_exit")]
+pub fn stub_19b06c() -> ! {
+    todo!("0x19b06c _TIFFjpeg_error_exit")
+}
+
+// 0x19b0c8 — _TIFFjpeg_create_compress
+#[doc(alias = "_TIFFjpeg_create_compress")]
+pub fn stub_19b0c8() -> ! {
+    todo!("0x19b0c8 _TIFFjpeg_create_compress")
+}
+
+// 0x19b138 — _JPEGInitializeLibJPEG
+#[doc(alias = "_JPEGInitializeLibJPEG")]
+pub fn stub_19b138() -> ! {
+    todo!("0x19b138 _JPEGInitializeLibJPEG")
+}
+
+// 0x19b294 — _TIFFjpeg_set_defaults
+#[doc(alias = "_TIFFjpeg_set_defaults")]
+pub fn stub_19b294() -> ! {
+    todo!("0x19b294 _TIFFjpeg_set_defaults")
+}
+
+// 0x19b2cc — _TIFFjpeg_suppress_tables
+#[doc(alias = "_TIFFjpeg_suppress_tables")]
+pub fn stub_19b2cc() -> ! {
+    todo!("0x19b2cc _TIFFjpeg_suppress_tables")
+}
+
+// 0x19b310 — _tables_empty_output_buffer
+#[doc(alias = "_tables_empty_output_buffer")]
+pub fn stub_19b310() -> ! {
+    todo!("0x19b310 _tables_empty_output_buffer")
+}
+
+// 0x19b384 — _TIFFjpeg_write_tables
+#[doc(alias = "_TIFFjpeg_write_tables")]
+pub fn stub_19b384() -> ! {
+    todo!("0x19b384 _TIFFjpeg_write_tables")
+}
+
+// 0x19b3bc — _JPEGSetupEncode
+#[doc(alias = "_JPEGSetupEncode")]
+pub fn stub_19b3bc() -> ! {
+    todo!("0x19b3bc _JPEGSetupEncode")
+}
+
+// 0x19b840 — _std_empty_output_buffer
+#[doc(alias = "_std_empty_output_buffer")]
+pub fn stub_19b840() -> ! {
+    todo!("0x19b840 _std_empty_output_buffer")
+}
+
+// 0x19b878 — _TIFFjpeg_read_scanlines
+#[doc(alias = "_TIFFjpeg_read_scanlines")]
+pub fn stub_19b878() -> ! {
+    todo!("0x19b878 _TIFFjpeg_read_scanlines")
+}
+
+// 0x19b8bc — _TIFFjpeg_finish_decompress
+#[doc(alias = "_TIFFjpeg_finish_decompress")]
+pub fn stub_19b8bc() -> ! {
+    todo!("0x19b8bc _TIFFjpeg_finish_decompress")
+}
+
+// 0x19b8f0 — _JPEGDecode
+#[doc(alias = "_JPEGDecode")]
+pub fn stub_19b8f0() -> ! {
+    todo!("0x19b8f0 _JPEGDecode")
+}
+
+// 0x19bcfc — _TIFFjpeg_read_header
+#[doc(alias = "_TIFFjpeg_read_header")]
+pub fn stub_19bcfc() -> ! {
+    todo!("0x19bcfc _TIFFjpeg_read_header")
+}
+
+// 0x19bd3c — _JPEGSetupDecode
+#[doc(alias = "_JPEGSetupDecode")]
+pub fn stub_19bd3c() -> ! {
+    todo!("0x19bd3c _JPEGSetupDecode")
+}
+
+// 0x19be84 — _TIFFjpeg_start_decompress
+#[doc(alias = "_TIFFjpeg_start_decompress")]
+pub fn stub_19be84() -> ! {
+    todo!("0x19be84 _TIFFjpeg_start_decompress")
+}
+
+// 0x19bebc — _JPEGPreDecode
+#[doc(alias = "_JPEGPreDecode")]
+pub fn stub_19bebc() -> ! {
+    todo!("0x19bebc _JPEGPreDecode")
+}
+
+// 0x19c4a0 — _TIFFjpeg_read_raw_data
+#[doc(alias = "_TIFFjpeg_read_raw_data")]
+pub fn stub_19c4a0() -> ! {
+    todo!("0x19c4a0 _TIFFjpeg_read_raw_data")
+}
+
+// 0x19c4e4 — _JPEGDecodeRaw
+#[doc(alias = "_JPEGDecodeRaw")]
+pub fn stub_19c4e4() -> ! {
+    todo!("0x19c4e4 _JPEGDecodeRaw")
+}
+
+// 0x19c8a0 — _JPEGPrintDir
+#[doc(alias = "_JPEGPrintDir")]
+pub fn stub_19c8a0() -> ! {
+    todo!("0x19c8a0 _JPEGPrintDir")
+}
+
+// 0x19c9a0 — _JPEGResetUpsampled
+#[doc(alias = "_JPEGResetUpsampled")]
+pub fn stub_19c9a0() -> ! {
+    todo!("0x19c9a0 _JPEGResetUpsampled")
+}
+
+// 0x19c9fc — _JPEGVSetField
+#[doc(alias = "_JPEGVSetField")]
+pub fn stub_19c9fc() -> ! {
+    todo!("0x19c9fc _JPEGVSetField")
+}
+
+// 0x19cc14 — _JPEGVGetField
+#[doc(alias = "_JPEGVGetField")]
+pub fn stub_19cc14() -> ! {
+    todo!("0x19cc14 _JPEGVGetField")
+}
+
+// 0x19ce10 — _JPEGPreEncode
+#[doc(alias = "_JPEGPreEncode")]
+pub fn stub_19ce10() -> ! {
+    todo!("0x19ce10 _JPEGPreEncode")
+}
+
+// 0x19d180 — _uv_decode
+#[doc(alias = "_uv_decode")]
+pub fn stub_19d180() -> ! {
+    todo!("0x19d180 _uv_decode")
+}
+
+// 0x19d260 — _Luv24toLuv48
+#[doc(alias = "_Luv24toLuv48")]
+pub fn stub_19d260() -> ! {
+    todo!("0x19d260 _Luv24toLuv48")
+}
+
+// 0x19d478 — _Luv32toLuv48
+#[doc(alias = "_Luv32toLuv48")]
+pub fn stub_19d478() -> ! {
+    todo!("0x19d478 _Luv32toLuv48")
+}
+
+// 0x19d5f8 — __logLuvNop
+#[doc(alias = "__logLuvNop")]
+pub fn stub_19d5f8() -> ! {
+    todo!("0x19d5f8 __logLuvNop")
+}
+
+// 0x19d5fc — _multiply
+#[doc(alias = "_multiply")]
+pub fn stub_19d5fc() -> ! {
+    todo!("0x19d5fc _multiply")
+}
+
+// 0x19d62c — _LogLuvClose
+#[doc(alias = "_LogLuvClose")]
+pub fn stub_19d62c() -> ! {
+    todo!("0x19d62c _LogLuvClose")
+}
+
+// 0x19d658 — _LogLuvVGetField
+#[doc(alias = "_LogLuvVGetField")]
+pub fn stub_19d658() -> ! {
+    todo!("0x19d658 _LogLuvVGetField")
+}
