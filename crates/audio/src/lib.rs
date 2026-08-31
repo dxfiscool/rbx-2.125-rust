d755c60c30a5fae0e70c2d76451bf2b3ebfe3862
dxf <dxf@local>
2026-08-31 10:09:16 +0200

    audio: next 100 stubs (0xe499c-0xf5a68, 1750/1882 FMOD, rbx_core::SharedPtr not boost) [skeleton batch]

--- crates/audio/src/lib.rs
+++ crates/audio/src/lib.rs
@@ -1,5 +1,5 @@
 //! audio — FMOD (FMOD::System, FMOD::Sound, Channel, etc.)
-//! Generated from ida/export.json filtered for FMOD (1882 funcs, 1550 stubs here).
+//! Generated from ida/export.json filtered for FMOD (1882 funcs, 1750 stubs here).
 //! Each stub preserves IDA ea + mangled + demangled for rg.
 
 // 0x686a4 — __ZN4FMOD10ProfileCpu4initEv
@@ -9796,3 +9796,603 @@ pub fn stub_e44c0() -> ! {
 pub fn stub_e45fc() -> ! {
     todo!("0xe45fc FMOD::SystemI::createChannelGroupInternal(char const*,FMOD::ChannelGroupI **,bool,bool)")
 }
+// 0xe499c — __ZN4FMOD7SystemI18createChannelGroupEPKcPPNS_13ChannelGroupIE
+#[doc(alias = "FMOD::SystemI::createChannelGroup(char const*,FMOD::ChannelGroupI **)")]
+pub fn stub_e499c() -> ! {
+    todo!("0xe499c FMOD::SystemI::createChannelGroup(char const*,FMOD::ChannelGroupI **)")
+}
+
+// 0xe49ec — __ZN4FMOD7SystemI9createDSPEP20FMOD_DSP_DESCRIPTIONPPNS_4DSPIE
+#[doc(alias = "FMOD::SystemI::createDSP(FMOD_DSP_DESCRIPTION *,FMOD::DSPI **)")]
+pub fn stub_e49ec() -> ! {
+    todo!("0xe49ec FMOD::SystemI::createDSP(FMOD_DSP_DESCRIPTION *,FMOD::DSPI **)")
+}
+
+// 0xe4b18 — __ZN4FMOD7SystemI15createDSPByTypeE13FMOD_DSP_TYPEPPNS_4DSPIE
+#[doc(alias = "FMOD::SystemI::createDSPByType(FMOD_DSP_TYPE,FMOD::DSPI **)")]
+pub fn stub_e4b18() -> ! {
+    todo!("0xe4b18 FMOD::SystemI::createDSPByType(FMOD_DSP_TYPE,FMOD::DSPI **)")
+}
+
+// 0xe4c48 — __ZN4FMOD7SystemI13updateStreamsEv
+#[doc(alias = "FMOD::SystemI::updateStreams(void)")]
+pub fn stub_e4c48() -> ! {
+    todo!("0xe4c48 FMOD::SystemI::updateStreams(void)")
+}
+
+// 0xe4dbc — __ZN4FMOD7SystemI12streamThreadEPv
+#[doc(alias = "FMOD::SystemI::streamThread(void *)")]
+pub fn stub_e4dbc() -> ! {
+    todo!("0xe4dbc FMOD::SystemI::streamThread(void *)")
+}
+
+// 0xe4dc0 — __ZN4FMOD7SystemI11getCPUUsageEPfS1_S1_S1_S1_
+#[doc(alias = "FMOD::SystemI::getCPUUsage(float *,float *,float *,float *,float *)")]
+pub fn stub_e4dc0() -> ! {
+    todo!("0xe4dc0 FMOD::SystemI::getCPUUsage(float *,float *,float *,float *,float *)")
+}
+
+// 0xe4eb8 — __ZN4FMOD7SystemI16allocateDSPCodecE17FMOD_SOUND_FORMATPPNS_8DSPCodecE
+#[doc(alias = "FMOD::SystemI::allocateDSPCodec(FMOD_SOUND_FORMAT,FMOD::DSPCodec **)")]
+pub fn stub_e4eb8() -> ! {
+    todo!("0xe4eb8 FMOD::SystemI::allocateDSPCodec(FMOD_SOUND_FORMAT,FMOD::DSPCodec **)")
+}
+
+// 0xe4efc — __ZN4FMOD7SystemI12setUpPluginsEv
+#[doc(alias = "FMOD::SystemI::setUpPlugins(void)")]
+pub fn stub_e4efc() -> ! {
+    todo!("0xe4efc FMOD::SystemI::setUpPlugins(void)")
+}
+
+// 0xe553c — __ZN4FMOD7SystemI9setOutputE15FMOD_OUTPUTTYPE
+#[doc(alias = "FMOD::SystemI::setOutput(FMOD_OUTPUTTYPE)")]
+pub fn stub_e553c() -> ! {
+    todo!("0xe553c FMOD::SystemI::setOutput(FMOD_OUTPUTTYPE)")
+}
+
+// 0xe5684 — __ZN4FMOD7SystemI19getHardwareChannelsEPiS1_S1_
+#[doc(alias = "FMOD::SystemI::getHardwareChannels(int *,int *,int *)")]
+pub fn stub_e5684() -> ! {
+    todo!("0xe5684 FMOD::SystemI::getHardwareChannels(int *,int *,int *)")
+}
+
+// 0xe5774 — __ZN4FMOD7SystemI15checkDriverListEb
+#[doc(alias = "FMOD::SystemI::checkDriverList(bool)")]
+pub fn stub_e5774() -> ! {
+    todo!("0xe5774 FMOD::SystemI::checkDriverList(bool)")
+}
+
+// 0xe5884 — __ZN4FMOD7SystemI13getNumDriversEPi
+#[doc(alias = "FMOD::SystemI::getNumDrivers(int *)")]
+pub fn stub_e5884() -> ! {
+    todo!("0xe5884 FMOD::SystemI::getNumDrivers(int *)")
+}
+
+// 0xe5910 — __ZN4FMOD7SystemI9setDriverEi
+#[doc(alias = "FMOD::SystemI::setDriver(int)")]
+pub fn stub_e5910() -> ! {
+    todo!("0xe5910 FMOD::SystemI::setDriver(int)")
+}
+
+// 0xe5c60 — __ZN4FMOD7SystemI13getDriverCapsEiPjPiS2_P16FMOD_SPEAKERMODE
+#[doc(alias = "FMOD::SystemI::getDriverCaps(int,unsigned int *,int *,int *,FMOD_SPEAKERMODE *)")]
+pub fn stub_e5c60() -> ! {
+    todo!("0xe5c60 FMOD::SystemI::getDriverCaps(int,unsigned int *,int *,int *,FMOD_SPEAKERMODE *)")
+}
+
+// 0xe5e80 — __ZN4FMOD7SystemI13getDriverInfoEiPciP9FMOD_GUID
+#[doc(alias = "FMOD::SystemI::getDriverInfo(int,char *,int,FMOD_GUID *)")]
+pub fn stub_e5e80() -> ! {
+    todo!("0xe5e80 FMOD::SystemI::getDriverInfo(int,char *,int,FMOD_GUID *)")
+}
+
+// 0xe5f98 — __ZN4FMOD7SystemI12createSampleEjP21FMOD_CODEC_WAVEFORMATPPNS_6SampleE
+#[doc(alias = "FMOD::SystemI::createSample(unsigned int,FMOD_CODEC_WAVEFORMAT *,FMOD::Sample **)")]
+pub fn stub_e5f98() -> ! {
+    todo!("0xe5f98 FMOD::SystemI::createSample(unsigned int,FMOD_CODEC_WAVEFORMAT *,FMOD::Sample **)")
+}
+
+// 0xe6594 — __ZN4FMOD7SystemI11findChannelE17FMOD_CHANNELINDEXPNS_6SoundIEPPNS_8ChannelIE
+#[doc(alias = "FMOD::SystemI::findChannel(FMOD_CHANNELINDEX,FMOD::SoundI *,FMOD::ChannelI **)")]
+pub fn stub_e6594() -> ! {
+    todo!("0xe6594 FMOD::SystemI::findChannel(FMOD_CHANNELINDEX,FMOD::SoundI *,FMOD::ChannelI **)")
+}
+
+// 0xe69e0 — __ZN4FMOD7SystemI17updateSoundGroupsEi
+#[doc(alias = "FMOD::SystemI::updateSoundGroups(int)")]
+pub fn stub_e69e0() -> ! {
+    todo!("0xe69e0 FMOD::SystemI::updateSoundGroups(int)")
+}
+
+// 0xe6be0 — __ZN4FMOD7SystemI14updateChannelsEi
+#[doc(alias = "FMOD::SystemI::updateChannels(int)")]
+pub fn stub_e6be0() -> ! {
+    todo!("0xe6be0 FMOD::SystemI::updateChannels(int)")
+}
+
+// 0xe7628 — __ZN4FMOD7SystemI6updateEv
+#[doc(alias = "FMOD::SystemI::update(void)")]
+pub fn stub_e7628() -> ! {
+    todo!("0xe7628 FMOD::SystemI::update(void)")
+}
+
+// 0xe78c8 — __ZN4FMOD7SystemI7closeExEb
+#[doc(alias = "FMOD::SystemI::closeEx(bool)")]
+pub fn stub_e78c8() -> ! {
+    todo!("0xe78c8 FMOD::SystemI::closeEx(bool)")
+}
+
+// 0xe7ed0 — __ZN4FMOD7SystemI5closeEv
+#[doc(alias = "FMOD::SystemI::close(void)")]
+pub fn stub_e7ed0() -> ! {
+    todo!("0xe7ed0 FMOD::SystemI::close(void)")
+}
+
+// 0xe7ed8 — __ZN4FMOD7SystemI11getInstanceEjPPS0_
+#[doc(alias = "FMOD::SystemI::getInstance(unsigned int,FMOD::SystemI**)")]
+pub fn stub_e7ed8() -> ! {
+    todo!("0xe7ed8 FMOD::SystemI::getInstance(unsigned int,FMOD::SystemI**)")
+}
+
+// 0xe7f50 — __ZN4FMOD7SystemI7stopDSPEPNS_4DSPIE
+#[doc(alias = "FMOD::SystemI::stopDSP(FMOD::DSPI *)")]
+pub fn stub_e7f50() -> ! {
+    todo!("0xe7f50 FMOD::SystemI::stopDSP(FMOD::DSPI *)")
+}
+
+// 0xe7fdc — __ZN4FMOD7SystemI22count3DPhysicalReverbsEv
+#[doc(alias = "FMOD::SystemI::count3DPhysicalReverbs(void)")]
+pub fn stub_e7fdc() -> ! {
+    todo!("0xe7fdc FMOD::SystemI::count3DPhysicalReverbs(void)")
+}
+
+// 0xe8024 — __ZN4FMOD7SystemI9playSoundE17FMOD_CHANNELINDEXPNS_6SoundIEbPPNS_8ChannelIE
+#[doc(alias = "FMOD::SystemI::playSound(FMOD_CHANNELINDEX,FMOD::SoundI *,bool,FMOD::ChannelI **)")]
+pub fn stub_e8024() -> ! {
+    todo!("0xe8024 FMOD::SystemI::playSound(FMOD_CHANNELINDEX,FMOD::SoundI *,bool,FMOD::ChannelI **)")
+}
+
+// 0xe82a0 — __ZN4FMOD7SystemI26flushDSPConnectionRequestsEb
+#[doc(alias = "FMOD::SystemI::flushDSPConnectionRequests(bool)")]
+pub fn stub_e82a0() -> ! {
+    todo!("0xe82a0 FMOD::SystemI::flushDSPConnectionRequests(bool)")
+}
+
+// 0xe853c — __ZN4FMOD7SystemI21count3DVirtualReverbsEv
+#[doc(alias = "FMOD::SystemI::count3DVirtualReverbs(void)")]
+pub fn stub_e853c() -> ! {
+    todo!("0xe853c FMOD::SystemI::count3DVirtualReverbs(void)")
+}
+
+// 0xe8584 — __ZN4FMOD7SystemI7releaseEv
+#[doc(alias = "FMOD::SystemI::release(void)")]
+pub fn stub_e8584() -> ! {
+    todo!("0xe8584 FMOD::SystemI::release(void)")
+}
+
+// 0xe8634 — __ZN4FMOD7SystemI19prepareSpeakerPairsEv
+#[doc(alias = "FMOD::SystemI::prepareSpeakerPairs(void)")]
+pub fn stub_e8634() -> ! {
+    todo!("0xe8634 FMOD::SystemI::prepareSpeakerPairs(void)")
+}
+
+// 0xe8b14 — __ZN4FMOD7SystemI15sortSpeakerListEv
+#[doc(alias = "FMOD::SystemI::sortSpeakerList(void)")]
+pub fn stub_e8b14() -> ! {
+    todo!("0xe8b14 FMOD::SystemI::sortSpeakerList(void)")
+}
+
+// 0xe8c80 — __ZN4FMOD7SystemI20set3DSpeakerPositionE12FMOD_SPEAKERffb
+#[doc(alias = "FMOD::SystemI::set3DSpeakerPosition(FMOD_SPEAKER,float,float,bool)")]
+pub fn stub_e8c80() -> ! {
+    todo!("0xe8c80 FMOD::SystemI::set3DSpeakerPosition(FMOD_SPEAKER,float,float,bool)")
+}
+
+// 0xe8d90 — __ZN4FMOD7SystemI14setSpeakerModeE16FMOD_SPEAKERMODE
+#[doc(alias = "FMOD::SystemI::setSpeakerMode(FMOD_SPEAKERMODE)")]
+pub fn stub_e8d90() -> ! {
+    todo!("0xe8d90 FMOD::SystemI::setSpeakerMode(FMOD_SPEAKERMODE)")
+}
+
+// 0xe90ac — __ZN4FMOD7SystemI4initEijPv
+#[doc(alias = "FMOD::SystemI::init(int,unsigned int,void *)")]
+pub fn stub_e90ac() -> ! {
+    todo!("0xe90ac FMOD::SystemI::init(int,unsigned int,void *)")
+}
+
+// 0xe9e54 — __ZN4FMOD7SystemIC2Ev
+#[doc(alias = "FMOD::SystemI::SystemI(void)")]
+pub fn stub_e9e54() -> ! {
+    todo!("0xe9e54 FMOD::SystemI::SystemI(void)")
+}
+
+// 0xea4ac — __ZN4FMOD7SystemIC1Ev
+#[doc(alias = "FMOD::SystemI::SystemI(void)")]
+pub fn stub_ea4ac() -> ! {
+    todo!("0xea4ac FMOD::SystemI::SystemI(void)")
+}
+
+// 0xea4b0 — __ZN4FMOD7SystemI19createSoundInternalEPKcjjjP22FMOD_CREATESOUNDEXINFObPPNS_6SoundIE
+#[doc(alias = "FMOD::SystemI::createSoundInternal(char const*,unsigned int,unsigned int,unsigned int,FMOD_CREATESOUNDEXINFO *,bool,FMOD::SoundI **)")]
+pub fn stub_ea4b0() -> ! {
+    todo!("0xea4b0 FMOD::SystemI::createSoundInternal(char const*,unsigned int,unsigned int,unsigned int,FMOD_CREATESOUNDEXINFO *,bool,FMOD::SoundI **)")
+}
+
+// 0xed7e4 — __ZN4FMOD7SystemI11createSoundEPKcjP22FMOD_CREATESOUNDEXINFOPPNS_6SoundIE
+#[doc(alias = "FMOD::SystemI::createSound(char const*,unsigned int,FMOD_CREATESOUNDEXINFO *,FMOD::SoundI **)")]
+pub fn stub_ed7e4() -> ! {
+    todo!("0xed7e4 FMOD::SystemI::createSound(char const*,unsigned int,FMOD_CREATESOUNDEXINFO *,FMOD::SoundI **)")
+}
+
+// 0xedce8 — __ZN4FMOD7SystemID0Ev
+#[doc(alias = "FMOD::SystemI::~SystemI()")]
+pub fn stub_edce8() -> ! {
+    todo!("0xedce8 FMOD::SystemI::~SystemI()")
+}
+
+// 0xedd54 — __ZN4FMOD7SystemID1Ev
+#[doc(alias = "FMOD::SystemI::~SystemI()")]
+pub fn stub_edd54() -> ! {
+    todo!("0xedd54 FMOD::SystemI::~SystemI()")
+}
+
+// 0xeddb8 — __ZN4FMOD6Thread10threadFuncEv
+#[doc(alias = "FMOD::Thread::threadFunc(void)")]
+pub fn stub_eddb8() -> ! {
+    todo!("0xeddb8 FMOD::Thread::threadFunc(void)")
+}
+
+// 0xeddc0 — __ZN4FMOD6ThreadC2Ev
+#[doc(alias = "FMOD::Thread::Thread(void)")]
+pub fn stub_eddc0() -> ! {
+    todo!("0xeddc0 FMOD::Thread::Thread(void)")
+}
+
+// 0xeddfc — __ZN4FMOD6ThreadC1Ev
+#[doc(alias = "FMOD::Thread::Thread(void)")]
+pub fn stub_eddfc() -> ! {
+    todo!("0xeddfc FMOD::Thread::Thread(void)")
+}
+
+// 0xede00 — __ZN4FMOD6Thread12wakeupThreadEb
+#[doc(alias = "FMOD::Thread::wakeupThread(bool)")]
+pub fn stub_ede00() -> ! {
+    todo!("0xede00 FMOD::Thread::wakeupThread(bool)")
+}
+
+// 0xede20 — __ZN4FMOD6Thread11closeThreadEv
+#[doc(alias = "FMOD::Thread::closeThread(void)")]
+pub fn stub_ede20() -> ! {
+    todo!("0xede20 FMOD::Thread::closeThread(void)")
+}
+
+// 0xedef4 — __ZN4FMOD6Thread8callbackEPv
+#[doc(alias = "FMOD::Thread::callback(void *)")]
+pub fn stub_edef4() -> ! {
+    todo!("0xedef4 FMOD::Thread::callback(void *)")
+}
+
+// 0xedf94 — __ZN4FMOD6Thread10initThreadEPKcPFvPvES3_NS0_8PRIORITYES3_ibiPNS_7SystemIE
+#[doc(alias = "FMOD::Thread::initThread(char const*,void (*)(void *),void *,FMOD::Thread::PRIORITY,void *,int,bool,int,FMOD::SystemI *)")]
+pub fn stub_edf94() -> ! {
+    todo!("0xedf94 FMOD::Thread::initThread(char const*,void (*)(void *),void *,FMOD::Thread::PRIORITY,void *,int,bool,int,FMOD::SystemI *)")
+}
+
+// 0xee144 — __ZN4FMOD6ThreadD0Ev
+#[doc(alias = "FMOD::Thread::~Thread()")]
+pub fn stub_ee144() -> ! {
+    todo!("0xee144 FMOD::Thread::~Thread()")
+}
+
+// 0xee168 — __ZN4FMOD6ThreadD1Ev
+#[doc(alias = "FMOD::Thread::~Thread()")]
+pub fn stub_ee168() -> ! {
+    todo!("0xee168 FMOD::Thread::~Thread()")
+}
+
+// 0xee180 — __ZN4FMOD9TimeStampC2Ev
+#[doc(alias = "FMOD::TimeStamp::TimeStamp(void)")]
+pub fn stub_ee180() -> ! {
+    todo!("0xee180 FMOD::TimeStamp::TimeStamp(void)")
+}
+
+// 0xee1b0 — __ZN4FMOD9TimeStampC1Ev
+#[doc(alias = "FMOD::TimeStamp::TimeStamp(void)")]
+pub fn stub_ee1b0() -> ! {
+    todo!("0xee1b0 FMOD::TimeStamp::TimeStamp(void)")
+}
+
+// 0xee1b4 — __ZN4FMOD9TimeStamp11getCPUUsageEPf
+#[doc(alias = "FMOD::TimeStamp::getCPUUsage(float *)")]
+pub fn stub_ee1b4() -> ! {
+    todo!("0xee1b4 FMOD::TimeStamp::getCPUUsage(float *)")
+}
+
+// 0xee1cc — __ZN4FMOD9TimeStamp9setPausedEb
+#[doc(alias = "FMOD::TimeStamp::setPaused(bool)")]
+pub fn stub_ee1cc() -> ! {
+    todo!("0xee1cc FMOD::TimeStamp::setPaused(bool)")
+}
+
+// 0xee260 — __ZN4FMOD9TimeStamp8stampOutEi
+#[doc(alias = "FMOD::TimeStamp::stampOut(int)")]
+pub fn stub_ee260() -> ! {
+    todo!("0xee260 FMOD::TimeStamp::stampOut(int)")
+}
+
+// 0xee344 — __ZN4FMOD9TimeStamp7stampInEv
+#[doc(alias = "FMOD::TimeStamp::stampIn(void)")]
+pub fn stub_ee344() -> ! {
+    todo!("0xee344 FMOD::TimeStamp::stampIn(void)")
+}
+
+// 0xee42c — _FMOD_ogg_page_version
+#[doc(alias = "_FMOD_ogg_page_version")]
+pub fn stub_ee42c() -> ! {
+    todo!("0xee42c _FMOD_ogg_page_version")
+}
+
+// 0xee438 — _FMOD_ogg_page_continued
+#[doc(alias = "_FMOD_ogg_page_continued")]
+pub fn stub_ee438() -> ! {
+    todo!("0xee438 _FMOD_ogg_page_continued")
+}
+
+// 0xee448 — _FMOD_ogg_page_bos
+#[doc(alias = "_FMOD_ogg_page_bos")]
+pub fn stub_ee448() -> ! {
+    todo!("0xee448 _FMOD_ogg_page_bos")
+}
+
+// 0xee458 — _FMOD_ogg_page_eos
+#[doc(alias = "_FMOD_ogg_page_eos")]
+pub fn stub_ee458() -> ! {
+    todo!("0xee458 _FMOD_ogg_page_eos")
+}
+
+// 0xee468 — _FMOD_ogg_page_granulepos
+#[doc(alias = "_FMOD_ogg_page_granulepos")]
+pub fn stub_ee468() -> ! {
+    todo!("0xee468 _FMOD_ogg_page_granulepos")
+}
+
+// 0xee53c — _FMOD_ogg_page_serialno
+#[doc(alias = "_FMOD_ogg_page_serialno")]
+pub fn stub_ee53c() -> ! {
+    todo!("0xee53c _FMOD_ogg_page_serialno")
+}
+
+// 0xee564 — _FMOD_ogg_page_pageno
+#[doc(alias = "_FMOD_ogg_page_pageno")]
+pub fn stub_ee564() -> ! {
+    todo!("0xee564 _FMOD_ogg_page_pageno")
+}
+
+// 0xee58c — _FMOD_ogg_stream_check
+#[doc(alias = "_FMOD_ogg_stream_check")]
+pub fn stub_ee58c() -> ! {
+    todo!("0xee58c _FMOD_ogg_stream_check")
+}
+
+// 0xee5a4 — _FMOD_ogg_page_checksum_set
+#[doc(alias = "_FMOD_ogg_page_checksum_set")]
+pub fn stub_ee5a4() -> ! {
+    todo!("0xee5a4 _FMOD_ogg_page_checksum_set")
+}
+
+// 0xee688 — _FMOD_ogg_sync_check
+#[doc(alias = "_FMOD_ogg_sync_check")]
+pub fn stub_ee688() -> ! {
+    todo!("0xee688 _FMOD_ogg_sync_check")
+}
+
+// 0xee69c — _FMOD_ogg_sync_wrote
+#[doc(alias = "_FMOD_ogg_sync_wrote")]
+pub fn stub_ee69c() -> ! {
+    todo!("0xee69c _FMOD_ogg_sync_wrote")
+}
+
+// 0xee6dc — _FMOD_ogg_sync_reset
+#[doc(alias = "_FMOD_ogg_sync_reset")]
+pub fn stub_ee6dc() -> ! {
+    todo!("0xee6dc _FMOD_ogg_sync_reset")
+}
+
+// 0xee70c — _FMOD_ogg_stream_reset
+#[doc(alias = "_FMOD_ogg_stream_reset")]
+pub fn stub_ee70c() -> ! {
+    todo!("0xee70c _FMOD_ogg_stream_reset")
+}
+
+// 0xee76c — _FMOD_ogg_stream_reset_serialno
+#[doc(alias = "_FMOD_ogg_stream_reset_serialno")]
+pub fn stub_ee76c() -> ! {
+    todo!("0xee76c _FMOD_ogg_stream_reset_serialno")
+}
+
+// 0xee8dc — _FMOD_ogg_stream_packetout
+#[doc(alias = "_FMOD_ogg_stream_packetout")]
+pub fn stub_ee8dc() -> ! {
+    todo!("0xee8dc _FMOD_ogg_stream_packetout")
+}
+
+// 0xee914 — _FMOD_ogg_stream_packetpeek
+#[doc(alias = "_FMOD_ogg_stream_packetpeek")]
+pub fn stub_ee914() -> ! {
+    todo!("0xee914 _FMOD_ogg_stream_packetpeek")
+}
+
+// 0xee948 — _FMOD_ogg_sync_clear
+#[doc(alias = "_FMOD_ogg_sync_clear")]
+pub fn stub_ee948() -> ! {
+    todo!("0xee948 _FMOD_ogg_sync_clear")
+}
+
+// 0xee998 — _FMOD_ogg_sync_buffer
+#[doc(alias = "_FMOD_ogg_sync_buffer")]
+pub fn stub_ee998() -> ! {
+    todo!("0xee998 _FMOD_ogg_sync_buffer")
+}
+
+// 0xeea80 — _FMOD_ogg_sync_init
+#[doc(alias = "_FMOD_ogg_sync_init")]
+pub fn stub_eea80() -> ! {
+    todo!("0xeea80 _FMOD_ogg_sync_init")
+}
+
+// 0xeeab8 — _FMOD_ogg_stream_clear
+#[doc(alias = "_FMOD_ogg_stream_clear")]
+pub fn stub_eeab8() -> ! {
+    todo!("0xeeab8 _FMOD_ogg_stream_clear")
+}
+
+// 0xeeb1c — _FMOD_ogg_stream_pagein
+#[doc(alias = "_FMOD_ogg_stream_pagein")]
+pub fn stub_eeb1c() -> ! {
+    todo!("0xeeb1c _FMOD_ogg_stream_pagein")
+}
+
+// 0xeefdc — _FMOD_ogg_stream_init
+#[doc(alias = "_FMOD_ogg_stream_init")]
+pub fn stub_eefdc() -> ! {
+    todo!("0xeefdc _FMOD_ogg_stream_init")
+}
+
+// 0xef088 — _FMOD_ogg_sync_pageseek
+#[doc(alias = "_FMOD_ogg_sync_pageseek")]
+pub fn stub_ef088() -> ! {
+    todo!("0xef088 _FMOD_ogg_sync_pageseek")
+}
+
+// 0xef274 — _FMOD_vorbis_info_blocksize
+#[doc(alias = "_FMOD_vorbis_info_blocksize")]
+pub fn stub_ef274() -> ! {
+    todo!("0xef274 _FMOD_vorbis_info_blocksize")
+}
+
+// 0xef2cc — _FMOD_vorbis_synthesis_idheader
+#[doc(alias = "_FMOD_vorbis_synthesis_idheader")]
+pub fn stub_ef2cc() -> ! {
+    todo!("0xef2cc _FMOD_vorbis_synthesis_idheader")
+}
+
+// 0xef35c — _FMOD_vorbis_comment_clear
+#[doc(alias = "_FMOD_vorbis_comment_clear")]
+pub fn stub_ef35c() -> ! {
+    todo!("0xef35c _FMOD_vorbis_comment_clear")
+}
+
+// 0xef408 — _FMOD_vorbis_comment_init
+#[doc(alias = "_FMOD_vorbis_comment_init")]
+pub fn stub_ef408() -> ! {
+    todo!("0xef408 _FMOD_vorbis_comment_init")
+}
+
+// 0xef428 — _FMOD_vorbis_info_clear
+#[doc(alias = "_FMOD_vorbis_info_clear")]
+pub fn stub_ef428() -> ! {
+    todo!("0xef428 _FMOD_vorbis_info_clear")
+}
+
+// 0xef630 — _FMOD_vorbis_synthesis_headerin
+#[doc(alias = "_FMOD_vorbis_synthesis_headerin")]
+pub fn stub_ef630() -> ! {
+    todo!("0xef630 _FMOD_vorbis_synthesis_headerin")
+}
+
+// 0xefd04 — _FMOD_vorbis_info_init
+#[doc(alias = "_FMOD_vorbis_info_init")]
+pub fn stub_efd04() -> ! {
+    todo!("0xefd04 _FMOD_vorbis_info_init")
+}
+
+// 0xf17a0 — _FMOD_mapping0_free_info
+#[doc(alias = "_FMOD_mapping0_free_info")]
+pub fn stub_f17a0() -> ! {
+    todo!("0xf17a0 _FMOD_mapping0_free_info")
+}
+
+// 0xf17d4 — _FMOD_mapping0_unpack
+#[doc(alias = "_FMOD_mapping0_unpack")]
+pub fn stub_f17d4() -> ! {
+    todo!("0xf17d4 _FMOD_mapping0_unpack")
+}
+
+// 0xf1a1c — _FMOD_mapping0_inverse
+#[doc(alias = "_FMOD_mapping0_inverse")]
+pub fn stub_f1a1c() -> ! {
+    todo!("0xf1a1c _FMOD_mapping0_inverse")
+}
+
+// 0xf2f34 — _FMOD_mdct_clear
+#[doc(alias = "_FMOD_mdct_clear")]
+pub fn stub_f2f34() -> ! {
+    todo!("0xf2f34 _FMOD_mdct_clear")
+}
+
+// 0xf2f90 — _FMOD_mdct_init
+#[doc(alias = "_FMOD_mdct_init")]
+pub fn stub_f2f90() -> ! {
+    todo!("0xf2f90 _FMOD_mdct_init")
+}
+
+// 0xf3bfc — _FMOD_mdct_backward
+#[doc(alias = "_FMOD_mdct_backward")]
+pub fn stub_f3bfc() -> ! {
+    todo!("0xf3bfc _FMOD_mdct_backward")
+}
+
+// 0xf4120 — _FMOD_res2_inverse
+#[doc(alias = "_FMOD_res2_inverse")]
+pub fn stub_f4120() -> ! {
+    todo!("0xf4120 _FMOD_res2_inverse")
+}
+
+// 0xf4374 — __FMOD_01inverse
+#[doc(alias = "__FMOD_01inverse")]
+pub fn stub_f4374() -> ! {
+    todo!("0xf4374 __FMOD_01inverse")
+}
+
+// 0xf4650 — _FMOD_res1_inverse
+#[doc(alias = "_FMOD_res1_inverse")]
+pub fn stub_f4650() -> ! {
+    todo!("0xf4650 _FMOD_res1_inverse")
+}
+
+// 0xf46dc — _FMOD_res0_inverse
+#[doc(alias = "_FMOD_res0_inverse")]
+pub fn stub_f46dc() -> ! {
+    todo!("0xf46dc _FMOD_res0_inverse")
+}
+
+// 0xf4768 — _FMOD_res0_look
+#[doc(alias = "_FMOD_res0_look")]
+pub fn stub_f4768() -> ! {
+    todo!("0xf4768 _FMOD_res0_look")
+}
+
+// 0xf4a4c — _FMOD_res0_free_look
+#[doc(alias = "_FMOD_res0_free_look")]
+pub fn stub_f4a4c() -> ! {
+    todo!("0xf4a4c _FMOD_res0_free_look")
+}
+
+// 0xf4b30 — _FMOD_res0_free_info
+#[doc(alias = "_FMOD_res0_free_info")]
+pub fn stub_f4b30() -> ! {
+    todo!("0xf4b30 _FMOD_res0_free_info")
+}
+
+// 0xf4b64 — _FMOD_res0_unpack
+#[doc(alias = "_FMOD_res0_unpack")]
+pub fn stub_f4b64() -> ! {
+    todo!("0xf4b64 _FMOD_res0_unpack")
+}
+
+// 0xf5a68 — __FMOD_ilog
+#[doc(alias = "__FMOD_ilog")]
+pub fn stub_f5a68() -> ! {
+    todo!("0xf5a68 __FMOD_ilog")
+}
+
