cb33631dd34b5ba1b8424d0697167a1a884406d0
dxf <dxf@local>
2026-09-01 18:33:20 +0200

    script: generated_sh_03 — 100 stubs 0x1b4944..0x1c4bbc [skeleton batch]

---
+++ crates/script/src/generated_sh_03.rs
@@ -0,0 +1,643 @@
+// Auto-generated skeletons for rbx-script — Lua/Script gap filler (generated_sh_03)
+// Filter: next 100 EA-sorted asc not yet stubbed in rbx-script (global gap filler)
+// Source: ida/export.json (85545 funcs, base 0x4000)
+// Batch: +100 stubs | range 0x1b4944..0x1c4bbc | script 18752->18852 distinct
+// SharedPtr = rbx_core::SharedPtr (Arc), not boost::shared_ptr; backtick stripped from alias
+
+#![allow(non_snake_case, dead_code, unused_variables, unused_imports, clippy::all)]
+
+use rbx_core::SharedPtr;
+
+// 0x1b4944 — _multiply_1
+// type: int __fastcall(_DWORD, _DWORD, _DWORD, _DWORD)
+#[doc(alias = "_multiply_1")]
+pub fn stub_1b4944() -> ! {
+    todo!("0x1b4944 _multiply_1")
+}
+
+// 0x1b49a4 — _TIFFOldScanlineSize
+#[doc(alias = "_TIFFOldScanlineSize")]
+pub fn stub_1b49a4() -> ! {
+    todo!("0x1b49a4 _TIFFOldScanlineSize")
+}
+
+// 0x1b4a08 — _TIFFNumberOfStrips
+#[doc(alias = "_TIFFNumberOfStrips")]
+pub fn stub_1b4a08() -> ! {
+    todo!("0x1b4a08 _TIFFNumberOfStrips")
+}
+
+// 0x1b4a68 — _summarize
+// type: int __fastcall(_DWORD, _DWORD, _DWORD, _DWORD)
+#[doc(alias = "_summarize")]
+pub fn stub_1b4a68() -> ! {
+    todo!("0x1b4a68 _summarize")
+}
+
+// 0x1b4a7c — _TIFFNewScanlineSize
+#[doc(alias = "_TIFFNewScanlineSize")]
+pub fn stub_1b4a7c() -> ! {
+    todo!("0x1b4a7c _TIFFNewScanlineSize")
+}
+
+// 0x1b4bb8 — _TIFFScanlineSize
+// type: int __fastcall(_DWORD)
+#[doc(alias = "_TIFFScanlineSize")]
+pub fn stub_1b4bb8() -> ! {
+    todo!("0x1b4bb8 _TIFFScanlineSize")
+}
+
+// 0x1b4d80 — __TIFFDefaultStripSize
+#[doc(alias = "__TIFFDefaultStripSize")]
+pub fn stub_1b4d80() -> ! {
+    todo!("0x1b4d80 __TIFFDefaultStripSize")
+}
+
+// 0x1b4dbc — _TIFFVStripSize
+// type: int __fastcall(int, int)
+#[doc(alias = "_TIFFVStripSize")]
+pub fn stub_1b4dbc() -> ! {
+    todo!("0x1b4dbc _TIFFVStripSize")
+}
+
+// 0x1b4f5c — _TIFFStripSize
+#[doc(alias = "_TIFFStripSize")]
+pub fn stub_1b4f5c() -> ! {
+    todo!("0x1b4f5c _TIFFStripSize")
+}
+
+// 0x1b4f70 — _TIFFSwabShort
+#[doc(alias = "_TIFFSwabShort")]
+pub fn stub_1b4f70() -> ! {
+    todo!("0x1b4f70 _TIFFSwabShort")
+}
+
+// 0x1b4f84 — _TIFFSwabLong
+#[doc(alias = "_TIFFSwabLong")]
+pub fn stub_1b4f84() -> ! {
+    todo!("0x1b4f84 _TIFFSwabLong")
+}
+
+// 0x1b4fa8 — _TIFFSwabArrayOfShort
+// type: int __fastcall(_DWORD, _DWORD)
+#[doc(alias = "_TIFFSwabArrayOfShort")]
+pub fn stub_1b4fa8() -> ! {
+    todo!("0x1b4fa8 _TIFFSwabArrayOfShort")
+}
+
+// 0x1b5118 — _TIFFSwabArrayOfTriples
+// type: int __fastcall(_DWORD)
+#[doc(alias = "_TIFFSwabArrayOfTriples")]
+pub fn stub_1b5118() -> ! {
+    todo!("0x1b5118 _TIFFSwabArrayOfTriples")
+}
+
+// 0x1b5288 — _TIFFSwabArrayOfLong
+// type: int __fastcall(_DWORD, _DWORD)
+#[doc(alias = "_TIFFSwabArrayOfLong")]
+pub fn stub_1b5288() -> ! {
+    todo!("0x1b5288 _TIFFSwabArrayOfLong")
+}
+
+// 0x1b5398 — _TIFFSwabArrayOfDouble
+// type: int __fastcall(_DWORD, _DWORD)
+#[doc(alias = "_TIFFSwabArrayOfDouble")]
+pub fn stub_1b5398() -> ! {
+    todo!("0x1b5398 _TIFFSwabArrayOfDouble")
+}
+
+// 0x1b54f8 — _TIFFGetBitRevTable
+#[doc(alias = "_TIFFGetBitRevTable")]
+pub fn stub_1b54f8() -> ! {
+    todo!("0x1b54f8 _TIFFGetBitRevTable")
+}
+
+// 0x1b5520 — _TIFFReverseBits
+#[doc(alias = "_TIFFReverseBits")]
+pub fn stub_1b5520() -> ! {
+    todo!("0x1b5520 _TIFFReverseBits")
+}
+
+// 0x1b55d8 — _TIFFInitThunderScan
+#[doc(alias = "_TIFFInitThunderScan")]
+pub fn stub_1b55d8() -> ! {
+    todo!("0x1b55d8 _TIFFInitThunderScan")
+}
+
+// 0x1b55f4 — _ThunderDecodeRow
+#[doc(alias = "_ThunderDecodeRow")]
+pub fn stub_1b55f4() -> ! {
+    todo!("0x1b55f4 _ThunderDecodeRow")
+}
+
+// 0x1b596c — _TIFFComputeTile
+#[doc(alias = "_TIFFComputeTile")]
+pub fn stub_1b596c() -> ! {
+    todo!("0x1b596c _TIFFComputeTile")
+}
+
+// 0x1b5ab8 — __TIFFDefaultTileSize
+#[doc(alias = "__TIFFDefaultTileSize")]
+pub fn stub_1b5ab8() -> ! {
+    todo!("0x1b5ab8 __TIFFDefaultTileSize")
+}
+
+// 0x1b5b04 — _TIFFCheckTile
+#[doc(alias = "_TIFFCheckTile")]
+pub fn stub_1b5b04() -> ! {
+    todo!("0x1b5b04 _TIFFCheckTile")
+}
+
+// 0x1b5bfc — _multiply_2
+#[doc(alias = "_multiply_2")]
+pub fn stub_1b5bfc() -> ! {
+    todo!("0x1b5bfc _multiply_2")
+}
+
+// 0x1b5c5c — _TIFFTileRowSize
+#[doc(alias = "_TIFFTileRowSize")]
+pub fn stub_1b5c5c() -> ! {
+    todo!("0x1b5c5c _TIFFTileRowSize")
+}
+
+// 0x1b5cdc — _TIFFNumberOfTiles
+#[doc(alias = "_TIFFNumberOfTiles")]
+pub fn stub_1b5cdc() -> ! {
+    todo!("0x1b5cdc _TIFFNumberOfTiles")
+}
+
+// 0x1b5dd8 — _TIFFVTileSize
+#[doc(alias = "_TIFFVTileSize")]
+pub fn stub_1b5dd8() -> ! {
+    todo!("0x1b5dd8 _TIFFVTileSize")
+}
+
+// 0x1b5f84 — _TIFFTileSize
+#[doc(alias = "_TIFFTileSize")]
+pub fn stub_1b5f84() -> ! {
+    todo!("0x1b5f84 _TIFFTileSize")
+}
+
+// 0x1b5f8c — _TIFFWarningExt
+// type: _DWORD (__fastcall **(int, char *, const char *, ...))(const char *, const char *, void *)
+#[doc(alias = "_TIFFWarningExt")]
+pub fn stub_1b5f8c() -> ! {
+    todo!("0x1b5f8c _TIFFWarningExt")
+}
+
+// 0x1b6008 — _TIFFAppendToStrip
+#[doc(alias = "_TIFFAppendToStrip")]
+pub fn stub_1b6008() -> ! {
+    todo!("0x1b6008 _TIFFAppendToStrip")
+}
+
+// 0x1b617c — _TIFFFlushData1
+#[doc(alias = "_TIFFFlushData1")]
+pub fn stub_1b617c() -> ! {
+    todo!("0x1b617c _TIFFFlushData1")
+}
+
+// 0x1b61fc — _TIFFWriteBufferSetup
+#[doc(alias = "_TIFFWriteBufferSetup")]
+pub fn stub_1b61fc() -> ! {
+    todo!("0x1b61fc _TIFFWriteBufferSetup")
+}
+
+// 0x1b62ec — _TIFFSetupStrips
+#[doc(alias = "_TIFFSetupStrips")]
+pub fn stub_1b62ec() -> ! {
+    todo!("0x1b62ec _TIFFSetupStrips")
+}
+
+// 0x1b63ec — _TIFFWriteCheck
+// type: int __fastcall(int, int, char *)
+#[doc(alias = "_TIFFWriteCheck")]
+pub fn stub_1b63ec() -> ! {
+    todo!("0x1b63ec _TIFFWriteCheck")
+}
+
+// 0x1b658c — _TIFFGrowStrips
+#[doc(alias = "_TIFFGrowStrips")]
+pub fn stub_1b658c() -> ! {
+    todo!("0x1b658c _TIFFGrowStrips")
+}
+
+// 0x1b66cc — _TIFFWriteScanline
+#[doc(alias = "_TIFFWriteScanline")]
+pub fn stub_1b66cc() -> ! {
+    todo!("0x1b66cc _TIFFWriteScanline")
+}
+
+// 0x1b6998 — _ZIPVGetField
+#[doc(alias = "_ZIPVGetField")]
+pub fn stub_1b6998() -> ! {
+    todo!("0x1b6998 _ZIPVGetField")
+}
+
+// 0x1b69d8 — _TIFFInitZIP
+#[doc(alias = "_TIFFInitZIP")]
+pub fn stub_1b69d8() -> ! {
+    todo!("0x1b69d8 _TIFFInitZIP")
+}
+
+// 0x1b6b94 — _ZIPCleanup
+#[doc(alias = "_ZIPCleanup")]
+pub fn stub_1b6b94() -> ! {
+    todo!("0x1b6b94 _ZIPCleanup")
+}
+
+// 0x1b6c3c — _ZIPPostEncode
+#[doc(alias = "_ZIPPostEncode")]
+pub fn stub_1b6c3c() -> ! {
+    todo!("0x1b6c3c _ZIPPostEncode")
+}
+
+// 0x1b6cf8 — _ZIPEncode
+#[doc(alias = "_ZIPEncode")]
+pub fn stub_1b6cf8() -> ! {
+    todo!("0x1b6cf8 _ZIPEncode")
+}
+
+// 0x1b6e10 — _ZIPPreEncode
+#[doc(alias = "_ZIPPreEncode")]
+pub fn stub_1b6e10() -> ! {
+    todo!("0x1b6e10 _ZIPPreEncode")
+}
+
+// 0x1b6e88 — _ZIPSetupEncode
+#[doc(alias = "_ZIPSetupEncode")]
+pub fn stub_1b6e88() -> ! {
+    todo!("0x1b6e88 _ZIPSetupEncode")
+}
+
+// 0x1b6f64 — _ZIPDecode
+#[doc(alias = "_ZIPDecode")]
+pub fn stub_1b6f64() -> ! {
+    todo!("0x1b6f64 _ZIPDecode")
+}
+
+// 0x1b70ec — _ZIPPreDecode
+#[doc(alias = "_ZIPPreDecode")]
+pub fn stub_1b70ec() -> ! {
+    todo!("0x1b70ec _ZIPPreDecode")
+}
+
+// 0x1b7164 — _ZIPSetupDecode
+#[doc(alias = "_ZIPSetupDecode")]
+pub fn stub_1b7164() -> ! {
+    todo!("0x1b7164 _ZIPSetupDecode")
+}
+
+// 0x1b723c — _ZIPVSetField
+#[doc(alias = "_ZIPVSetField")]
+pub fn stub_1b723c() -> ! {
+    todo!("0x1b723c _ZIPVSetField")
+}
+
+// 0x1b72d8 — _adler32
+// type: uLong __cdecl(uLong adler, const Bytef *buf, uInt len)
+#[doc(alias = "_adler32")]
+pub fn stub_1b72d8() -> ! {
+    todo!("0x1b72d8 _adler32")
+}
+
+// 0x1b7acc — _crc32
+// type: uLong __cdecl(uLong crc, const Bytef *buf, uInt len)
+#[doc(alias = "_crc32")]
+pub fn stub_1b7acc() -> ! {
+    todo!("0x1b7acc _crc32")
+}
+
+// 0x1b84e8 — _putShortMSB
+// type: int __fastcall(_DWORD, _DWORD)
+#[doc(alias = "_putShortMSB")]
+pub fn stub_1b84e8() -> ! {
+    todo!("0x1b84e8 _putShortMSB")
+}
+
+// 0x1b8510 — _deflateEnd
+// type: int __cdecl(z_streamp strm)
+#[doc(alias = "_deflateEnd")]
+pub fn stub_1b8510() -> ! {
+    todo!("0x1b8510 _deflateEnd")
+}
+
+// 0x1b8608 — _longest_match
+#[doc(alias = "_longest_match")]
+pub fn stub_1b8608() -> ! {
+    todo!("0x1b8608 _longest_match")
+}
+
+// 0x1b8980 — _longest_match_fast
+#[doc(alias = "_longest_match_fast")]
+pub fn stub_1b8980() -> ! {
+    todo!("0x1b8980 _longest_match_fast")
+}
+
+// 0x1b8ab8 — _flush_pending
+// type: int __fastcall(_DWORD)
+#[doc(alias = "_flush_pending")]
+pub fn stub_1b8ab8() -> ! {
+    todo!("0x1b8ab8 _flush_pending")
+}
+
+// 0x1b8b50 — _fill_window
+#[doc(alias = "_fill_window")]
+pub fn stub_1b8b50() -> ! {
+    todo!("0x1b8b50 _fill_window")
+}
+
+// 0x1b915c — _deflate
+// type: int __cdecl(z_streamp strm, int flush)
+#[doc(alias = "_deflate")]
+pub fn stub_1b915c() -> ! {
+    todo!("0x1b915c _deflate")
+}
+
+// 0x1b9c44 — _deflateParams
+// type: int __cdecl(z_streamp strm, int level, int strategy)
+#[doc(alias = "_deflateParams")]
+pub fn stub_1b9c44() -> ! {
+    todo!("0x1b9c44 _deflateParams")
+}
+
+// 0x1b9d44 — _deflate_slow
+#[doc(alias = "_deflate_slow")]
+pub fn stub_1b9d44() -> ! {
+    todo!("0x1b9d44 _deflate_slow")
+}
+
+// 0x1ba298 — _deflate_fast
+#[doc(alias = "_deflate_fast")]
+pub fn stub_1ba298() -> ! {
+    todo!("0x1ba298 _deflate_fast")
+}
+
+// 0x1ba6cc — _deflate_stored
+#[doc(alias = "_deflate_stored")]
+pub fn stub_1ba6cc() -> ! {
+    todo!("0x1ba6cc _deflate_stored")
+}
+
+// 0x1ba874 — _deflateReset
+// type: int __cdecl(z_streamp strm)
+#[doc(alias = "_deflateReset")]
+pub fn stub_1ba874() -> ! {
+    todo!("0x1ba874 _deflateReset")
+}
+
+// 0x1ba9c4 — _deflateInit2_
+// type: int __cdecl(z_streamp strm, int level, int method, int windowBits, int memLevel, int strategy, const char *version, int stream_size)
+#[doc(alias = "_deflateInit2_")]
+pub fn stub_1ba9c4() -> ! {
+    todo!("0x1ba9c4 _deflateInit2_")
+}
+
+// 0x1baca4 — _deflateInit_
+// type: int __cdecl(z_streamp strm, int level, const char *version, int stream_size)
+#[doc(alias = "_deflateInit_")]
+pub fn stub_1baca4() -> ! {
+    todo!("0x1baca4 _deflateInit_")
+}
+
+// 0x1bacdc — _inflate_fast
+#[doc(alias = "_inflate_fast")]
+pub fn stub_1bacdc() -> ! {
+    todo!("0x1bacdc _inflate_fast")
+}
+
+// 0x1bb908 — _inflateReset
+// type: int __cdecl(z_streamp strm)
+#[doc(alias = "_inflateReset")]
+pub fn stub_1bb908() -> ! {
+    todo!("0x1bb908 _inflateReset")
+}
+
+// 0x1bb980 — _inflateInit2_
+// type: int __cdecl(z_streamp strm, int windowBits, const char *version, int stream_size)
+#[doc(alias = "_inflateInit2_")]
+pub fn stub_1bb980() -> ! {
+    todo!("0x1bb980 _inflateInit2_")
+}
+
+// 0x1bba84 — _inflateInit_
+// type: int __cdecl(z_streamp strm, const char *version, int stream_size)
+#[doc(alias = "_inflateInit_")]
+pub fn stub_1bba84() -> ! {
+    todo!("0x1bba84 _inflateInit_")
+}
+
+// 0x1bba98 — _inflateEnd
+// type: int __cdecl(z_streamp strm)
+#[doc(alias = "_inflateEnd")]
+pub fn stub_1bba98() -> ! {
+    todo!("0x1bba98 _inflateEnd")
+}
+
+// 0x1bbaf8 — _syncsearch
+#[doc(alias = "_syncsearch")]
+pub fn stub_1bbaf8() -> ! {
+    todo!("0x1bbaf8 _syncsearch")
+}
+
+// 0x1bbb50 — _inflateSync
+// type: int __cdecl(z_streamp strm)
+#[doc(alias = "_inflateSync")]
+pub fn stub_1bbb50() -> ! {
+    todo!("0x1bbb50 _inflateSync")
+}
+
+// 0x1bbc80 — _updatewindow
+#[doc(alias = "_updatewindow")]
+pub fn stub_1bbc80() -> ! {
+    todo!("0x1bbc80 _updatewindow")
+}
+
+// 0x1bbdb4 — _inflate
+// type: int __cdecl(z_streamp strm, int flush)
+#[doc(alias = "_inflate")]
+pub fn stub_1bbdb4() -> ! {
+    todo!("0x1bbdb4 _inflate")
+}
+
+// 0x1c049c — _inflate_table
+#[doc(alias = "_inflate_table")]
+pub fn stub_1c049c() -> ! {
+    todo!("0x1c049c _inflate_table")
+}
+
+// 0x1c14c8 — _init_block
+#[doc(alias = "_init_block")]
+pub fn stub_1c14c8() -> ! {
+    todo!("0x1c14c8 _init_block")
+}
+
+// 0x1c16c4 — __tr_init
+#[doc(alias = "__tr_init")]
+pub fn stub_1c16c4() -> ! {
+    todo!("0x1c16c4 __tr_init")
+}
+
+// 0x1c173c — _pqdownheap
+#[doc(alias = "_pqdownheap")]
+pub fn stub_1c173c() -> ! {
+    todo!("0x1c173c _pqdownheap")
+}
+
+// 0x1c183c — _scan_tree
+#[doc(alias = "_scan_tree")]
+pub fn stub_1c183c() -> ! {
+    todo!("0x1c183c _scan_tree")
+}
+
+// 0x1c1b68 — _send_tree
+#[doc(alias = "_send_tree")]
+pub fn stub_1c1b68() -> ! {
+    todo!("0x1c1b68 _send_tree")
+}
+
+// 0x1c2304 — _compress_block
+#[doc(alias = "_compress_block")]
+pub fn stub_1c2304() -> ! {
+    todo!("0x1c2304 _compress_block")
+}
+
+// 0x1c2794 — _build_tree
+#[doc(alias = "_build_tree")]
+pub fn stub_1c2794() -> ! {
+    todo!("0x1c2794 _build_tree")
+}
+
+// 0x1c347c — _bi_flush
+#[doc(alias = "_bi_flush")]
+pub fn stub_1c347c() -> ! {
+    todo!("0x1c347c _bi_flush")
+}
+
+// 0x1c3514 — __tr_align
+// type: int __fastcall(_DWORD)
+#[doc(alias = "__tr_align")]
+pub fn stub_1c3514() -> ! {
+    todo!("0x1c3514 __tr_align")
+}
+
+// 0x1c37a0 — _bi_windup
+#[doc(alias = "_bi_windup")]
+pub fn stub_1c37a0() -> ! {
+    todo!("0x1c37a0 _bi_windup")
+}
+
+// 0x1c3818 — __tr_stored_block
+// type: int __fastcall(_DWORD, _DWORD, _DWORD, _DWORD)
+#[doc(alias = "__tr_stored_block")]
+pub fn stub_1c3818() -> ! {
+    todo!("0x1c3818 __tr_stored_block")
+}
+
+// 0x1c3ac4 — __tr_flush_block
+#[doc(alias = "__tr_flush_block")]
+pub fn stub_1c3ac4() -> ! {
+    todo!("0x1c3ac4 __tr_flush_block")
+}
+
+// 0x1c4270 — _uncompress
+// type: int __cdecl(Bytef *dest, uLongf *destLen, const Bytef *source, uLong sourceLen)
+#[doc(alias = "_uncompress")]
+pub fn stub_1c4270() -> ! {
+    todo!("0x1c4270 _uncompress")
+}
+
+// 0x1c4324 — _zError
+// type: const char *__cdecl(int)
+#[doc(alias = "_zError")]
+pub fn stub_1c4324() -> ! {
+    todo!("0x1c4324 _zError")
+}
+
+// 0x1c433c — _zcfree
+// type: int __fastcall(int, void *)
+#[doc(alias = "_zcfree")]
+pub fn stub_1c433c() -> ! {
+    todo!("0x1c433c _zcfree")
+}
+
+// 0x1c4350 — _zcalloc
+#[doc(alias = "_zcalloc")]
+pub fn stub_1c4350() -> ! {
+    todo!("0x1c4350 _zcalloc")
+}
+
+// 0x1c4364 — __ZN6TagLib17getFreeImageModelENS_7MDMODELE
+#[doc(alias = "TagLib::getFreeImageModel(TagLib::MDMODEL)")]
+pub fn stub_1c4364() -> ! {
+    todo!("0x1c4364 __ZN6TagLib17getFreeImageModelENS_7MDMODELE")
+}
+
+// 0x1c4410 — __ZN6TagLib8getTagIDENS_7MDMODELEPKc
+#[doc(alias = "TagLib::getTagID(TagLib::MDMODEL,char const*)")]
+pub fn stub_1c4410() -> ! {
+    todo!("0x1c4410 __ZN6TagLib8getTagIDENS_7MDMODELEPKc")
+}
+
+// 0x1c4494 — __ZN6TagLib10getTagInfoENS_7MDMODELEt
+#[doc(alias = "TagLib::getTagInfo(TagLib::MDMODEL,unsigned short)")]
+pub fn stub_1c4494() -> ! {
+    todo!("0x1c4494 __ZN6TagLib10getTagInfoENS_7MDMODELEt")
+}
+
+// 0x1c44f0 — __ZN6TagLib15getTagFieldNameENS_7MDMODELEtPc
+#[doc(alias = "TagLib::getTagFieldName(TagLib::MDMODEL,unsigned short,char *)")]
+pub fn stub_1c44f0() -> ! {
+    todo!("0x1c44f0 __ZN6TagLib15getTagFieldNameENS_7MDMODELEtPc")
+}
+
+// 0x1c4540 — __ZN6TagLib16addMetadataModelENS_7MDMODELEP10tagTagInfo
+#[doc(alias = "TagLib::addMetadataModel(TagLib::MDMODEL,tagTagInfo *)")]
+pub fn stub_1c4540() -> ! {
+    todo!("0x1c4540 __ZN6TagLib16addMetadataModelENS_7MDMODELEP10tagTagInfo")
+}
+
+// 0x1c45f0 — __ZN6TagLibC2Ev
+// type: TagLib *__fastcall(TagLib *__hidden this)
+#[doc(alias = "TagLib::TagLib(void)")]
+pub fn stub_1c45f0() -> ! {
+    todo!("0x1c45f0 __ZN6TagLibC2Ev")
+}
+
+// 0x1c48c4 — __ZN6TagLib8instanceEv
+// type: _DWORD __fastcall(TagLib *__hidden this)
+#[doc(alias = "TagLib::instance(void)")]
+pub fn stub_1c48c4() -> ! {
+    todo!("0x1c48c4 __ZN6TagLib8instanceEv")
+}
+
+// 0x1c49e4 — __ZN6TagLibD2Ev
+// type: void __fastcall(TagLib *__hidden this)
+#[doc(alias = "TagLib::~TagLib()")]
+pub fn stub_1c49e4() -> ! {
+    todo!("0x1c49e4 __ZN6TagLibD2Ev")
+}
+
+// 0x1c4b38 — ___tcf_0_0
+#[doc(alias = "___tcf_0_0")]
+pub fn stub_1c4b38() -> ! {
+    todo!("0x1c4b38 ___tcf_0_0")
+}
+
+// 0x1c4b48 — __ZNSt8_Rb_treeIiSt4pairIKiPSt3mapItP10tagTagInfoSt4lessItESaIS0_IKtS4_EEEESt10_Select1stISC_ES5_IiESaISC_EE13_Rb_tree_implISF_Lb0EEC2ERKSaISt13_Rb_tree_nodeISC_EERKSF_
+#[doc(alias = "std::_Rb_tree<int,std::pair<int const,std::map<unsigned short,tagTagInfo *,std::less<unsigned short>,std::allocator<std::pair<unsigned short const,tagTagInfo *>>> *>,std::_Select1st<std::pair<int const,std::map<unsigned short,tagTagInfo *,std::less<unsigned short>,std::allocator<std::pair<unsigned short const,tagTagInfo *>>> *>>,std::less<int>,std::allocator<std::pair<int const,std::map<unsigned short,tagTagInfo *,std::less<unsigned short>,std::allocator<std::pair<unsigned short const,tagTagInfo *>>> *>>>::_Rb_tree_impl<std::less<int>,false>::_Rb_tree_impl(std::allocator<std::_Rb_tree_node<std::pair<int const,std::map<unsigned short,tagTagInfo *,std::less<unsigned short>,std::allocator<std::pair<unsigned short const,tagTagInfo *>>> *>>> const&,std::less<int> const&)")]
+pub fn stub_1c4b48() -> ! {
+    todo!("0x1c4b48 __ZNSt8_Rb_treeIiSt4pairIKiPSt3mapItP10tagTagInfoSt4lessItESaIS0_IKtS4_EEEESt10_Select1stISC_ES5_IiESaISC_EE13_Rb_tree_implISF_Lb0EEC2ERKSaISt13_Rb_tree_nodeISC_EERKSF_")
+}
+
+// 0x1c4b88 — __ZNSt8_Rb_treeIiSt4pairIKiPSt3mapItP10tagTagInfoSt4lessItESaIS0_IKtS4_EEEESt10_Select1stISC_ES5_IiESaISC_EE11lower_boundERS1_
+#[doc(alias = "std::_Rb_tree<int,std::pair<int const,std::map<unsigned short,tagTagInfo *,std::less<unsigned short>,std::allocator<std::pair<unsigned short const,tagTagInfo *>>> *>,std::_Select1st<std::pair<int const,std::map<unsigned short,tagTagInfo *,std::less<unsigned short>,std::allocator<std::pair<unsigned short const,tagTagInfo *>>> *>>,std::less<int>,std::allocator<std::pair<int const,std::map<unsigned short,tagTagInfo *,std::less<unsigned short>,std::allocator<std::pair<unsigned short const,tagTagInfo *>>> *>>>::lower_bound(int const&)")]
+pub fn stub_1c4b88() -> ! {
+    todo!("0x1c4b88 __ZNSt8_Rb_treeIiSt4pairIKiPSt3mapItP10tagTagInfoSt4lessItESaIS0_IKtS4_EEEESt10_Select1stISC_ES5_IiESaISC_EE11lower_boundERS1_")
+}
+
+// 0x1c4bbc — __ZNSt8_Rb_treeItSt4pairIKtP10tagTagInfoESt10_Select1stIS4_ESt4lessItESaIS4_EE13_Rb_tree_implIS8_Lb0EEC2ERKSaISt13_Rb_tree_nodeIS4_EERKS8_
+#[doc(alias = "std::_Rb_tree<unsigned short,std::pair<unsigned short const,tagTagInfo *>,std::_Select1st<std::pair<unsigned short const,tagTagInfo *>>,std::less<unsigned short>,std::allocator<std::pair<unsigned short const,tagTagInfo *>>>::_Rb_tree_impl<std::less<unsigned short>,false>::_Rb_tree_impl(std::allocator<std::_Rb_tree_node<std::pair<unsigned short const,tagTagInfo *>>> const&,std::less<unsigned short> const&)")]
+pub fn stub_1c4bbc() -> ! {
+    todo!("0x1c4bbc __ZNSt8_Rb_treeItSt4pairIKtP10tagTagInfoESt10_Select1stIS4_ESt4lessItESaIS4_EE13_Rb_tree_implIS8_Lb0EEC2ERKSaISt13_Rb_tree_nodeIS4_EERKS8_")
+}
