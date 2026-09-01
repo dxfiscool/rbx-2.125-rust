001102aaa67ab89c4fc2a1496e83f1166e4f34c0
dxf <dxf@local>
2026-09-01 22:58:02 +0200

    datamodel: shard 241 — 100 stubs 0x180440..0x18c6f0 EA-sorted asc fallback gap filler not yet in datamodel (RBX::Instance|DataModel|Workspace primary complete, 27402->27502 distinct, rbx_core::SharedPtr not boost, // 0xADDR mangled + doc alias + todo) [skeleton batch]

---
+++ crates/datamodel/src/generated_241.rs
@@ -0,0 +1,626 @@
+// Auto-generated skeletons for rbx-datamodel — from ida/export.json
+// Filter: RBX::(Instance|DataModel|Workspace) EA-sorted asc next 100 not yet covered in ANY crate (fallback EA-sorted asc not yet in datamodel, global 85545 all covered)
+// Source: ida/export.json (85545 funcs, base 0x4000)
+// Batch: 100 stubs | range 0x180440..0x18c6f0 | global missing 0 (datamodel distinct 27402->27502, global 85545)
+// SharedPtr = rbx_core::SharedPtr (Arc), not boost::shared_ptr; ` and ' stripped from alias where needed
+
+#![allow(non_snake_case, dead_code, unused_variables, unused_imports, clippy::all)]
+
+use rbx_core::SharedPtr;
+const _SHARED_PTR: Option<SharedPtr<u8>> = None;
+
+
+// 0x180440 — _NotConfigured
+#[doc(alias = "_NotConfigured")]
+pub fn stub_180440() -> ! {
+    todo!("0x180440 _NotConfigured")
+}
+
+// 0x180468 — __notConfigured
+#[doc(alias = "__notConfigured")]
+pub fn stub_180468() -> ! {
+    todo!("0x180468 __notConfigured")
+}
+
+// 0x1804d8 — _TIFFCIELabToXYZ
+#[doc(alias = "_TIFFCIELabToXYZ")]
+pub fn stub_1804d8() -> ! {
+    todo!("0x1804d8 _TIFFCIELabToXYZ")
+}
+
+// 0x180640 — _TIFFXYZToRGB
+#[doc(alias = "_TIFFXYZToRGB")]
+pub fn stub_180640() -> ! {
+    todo!("0x180640 _TIFFXYZToRGB")
+}
+
+// 0x1808a8 — _TIFFYCbCrtoRGB
+#[doc(alias = "_TIFFYCbCrtoRGB")]
+pub fn stub_1808a8() -> ! {
+    todo!("0x1808a8 _TIFFYCbCrtoRGB")
+}
+
+// 0x18094c — _TIFFYCbCrToRGBInit
+#[doc(alias = "_TIFFYCbCrToRGBInit")]
+pub fn stub_18094c() -> ! {
+    todo!("0x18094c _TIFFYCbCrToRGBInit")
+}
+
+// 0x180c58 — _TIFFCIELabToRGBInit
+#[doc(alias = "_TIFFCIELabToRGBInit")]
+pub fn stub_180c58() -> ! {
+    todo!("0x180c58 _TIFFCIELabToRGBInit")
+}
+
+// 0x1812f8 — __TIFFNoPreCode
+#[doc(alias = "__TIFFNoPreCode")]
+pub fn stub_1812f8() -> ! {
+    todo!("0x1812f8 __TIFFNoPreCode")
+}
+
+// 0x181300 — __TIFFtrue
+#[doc(alias = "__TIFFtrue")]
+pub fn stub_181300() -> ! {
+    todo!("0x181300 __TIFFtrue")
+}
+
+// 0x181308 — __TIFFvoid
+#[doc(alias = "__TIFFvoid")]
+pub fn stub_181308() -> ! {
+    todo!("0x181308 __TIFFvoid")
+}
+
+// 0x18130c — __TIFFSetDefaultCompressionState
+// type: int __fastcall(_DWORD)
+#[doc(alias = "__TIFFSetDefaultCompressionState")]
+pub fn stub_18130c() -> ! {
+    todo!("0x18130c __TIFFSetDefaultCompressionState")
+}
+
+// 0x181400 — _TIFFFindCODEC
+// type: int __fastcall(_DWORD)
+#[doc(alias = "_TIFFFindCODEC")]
+pub fn stub_181400() -> ! {
+    todo!("0x181400 _TIFFFindCODEC")
+}
+
+// 0x181464 — _TIFFSetCompressionScheme
+#[doc(alias = "_TIFFSetCompressionScheme")]
+pub fn stub_181464() -> ! {
+    todo!("0x181464 _TIFFSetCompressionScheme")
+}
+
+// 0x1814ac — __TIFFNoSeek
+#[doc(alias = "__TIFFNoSeek")]
+pub fn stub_1814ac() -> ! {
+    todo!("0x1814ac __TIFFNoSeek")
+}
+
+// 0x1814d4 — _TIFFNoDecode
+// type: int __fastcall(int)
+#[doc(alias = "_TIFFNoDecode")]
+pub fn stub_1814d4() -> ! {
+    todo!("0x1814d4 _TIFFNoDecode")
+}
+
+// 0x181544 — __TIFFNoTileDecode
+#[doc(alias = "__TIFFNoTileDecode")]
+pub fn stub_181544() -> ! {
+    todo!("0x181544 __TIFFNoTileDecode")
+}
+
+// 0x181554 — __TIFFNoStripDecode
+#[doc(alias = "__TIFFNoStripDecode")]
+pub fn stub_181554() -> ! {
+    todo!("0x181554 __TIFFNoStripDecode")
+}
+
+// 0x181564 — __TIFFNoRowDecode
+#[doc(alias = "__TIFFNoRowDecode")]
+pub fn stub_181564() -> ! {
+    todo!("0x181564 __TIFFNoRowDecode")
+}
+
+// 0x181574 — _TIFFNoEncode
+#[doc(alias = "_TIFFNoEncode")]
+pub fn stub_181574() -> ! {
+    todo!("0x181574 _TIFFNoEncode")
+}
+
+// 0x1815e4 — __TIFFNoTileEncode
+#[doc(alias = "__TIFFNoTileEncode")]
+pub fn stub_1815e4() -> ! {
+    todo!("0x1815e4 __TIFFNoTileEncode")
+}
+
+// 0x1815f4 — __TIFFNoStripEncode
+#[doc(alias = "__TIFFNoStripEncode")]
+pub fn stub_1815f4() -> ! {
+    todo!("0x1815f4 __TIFFNoStripEncode")
+}
+
+// 0x181604 — __TIFFNoRowEncode
+#[doc(alias = "__TIFFNoRowEncode")]
+pub fn stub_181604() -> ! {
+    todo!("0x181604 __TIFFNoRowEncode")
+}
+
+// 0x181614 — _TIFFSetTagExtender
+#[doc(alias = "_TIFFSetTagExtender")]
+pub fn stub_181614() -> ! {
+    todo!("0x181614 _TIFFSetTagExtender")
+}
+
+// 0x181630 — _TIFFAdvanceDirectory
+#[doc(alias = "_TIFFAdvanceDirectory")]
+pub fn stub_181630() -> ! {
+    todo!("0x181630 _TIFFAdvanceDirectory")
+}
+
+// 0x181854 — _TIFFSetDirectory
+#[doc(alias = "_TIFFSetDirectory")]
+pub fn stub_181854() -> ! {
+    todo!("0x181854 _TIFFSetDirectory")
+}
+
+// 0x1818d8 — _TIFFFreeDirectory
+#[doc(alias = "_TIFFFreeDirectory")]
+pub fn stub_1818d8() -> ! {
+    todo!("0x1818d8 _TIFFFreeDirectory")
+}
+
+// 0x181a68 — _TIFFVGetField
+// type: int(void)
+#[doc(alias = "_TIFFVGetField")]
+pub fn stub_181a68() -> ! {
+    todo!("0x181a68 _TIFFVGetField")
+}
+
+// 0x181ad8 — _TIFFVSetField
+#[doc(alias = "_TIFFVSetField")]
+pub fn stub_181ad8() -> ! {
+    todo!("0x181ad8 _TIFFVSetField")
+}
+
+// 0x181bd0 — __TIFFVGetField
+#[doc(alias = "__TIFFVGetField")]
+pub fn stub_181bd0() -> ! {
+    todo!("0x181bd0 __TIFFVGetField")
+}
+
+// 0x18257c — _TIFFGetField
+// type: int __fastcall(_DWORD, _DWORD, _DWORD, _DWORD)
+#[doc(alias = "_TIFFGetField")]
+pub fn stub_18257c() -> ! {
+    todo!("0x18257c _TIFFGetField")
+}
+
+// 0x1825b0 — _TIFFSetField
+#[doc(alias = "_TIFFSetField")]
+pub fn stub_1825b0() -> ! {
+    todo!("0x1825b0 _TIFFSetField")
+}
+
+// 0x1825e4 — _TIFFDefaultDirectory
+#[doc(alias = "_TIFFDefaultDirectory")]
+pub fn stub_1825e4() -> ! {
+    todo!("0x1825e4 _TIFFDefaultDirectory")
+}
+
+// 0x1826f0 — _TIFFCreateDirectory
+#[doc(alias = "_TIFFCreateDirectory")]
+pub fn stub_1826f0() -> ! {
+    todo!("0x1826f0 _TIFFCreateDirectory")
+}
+
+// 0x182720 — _setByteArray
+// type: int __fastcall(_DWORD, _DWORD, _DWORD, _DWORD)
+#[doc(alias = "_setByteArray")]
+pub fn stub_182720() -> ! {
+    todo!("0x182720 _setByteArray")
+}
+
+// 0x1827a8 — __TIFFsetLongArray
+// type: int __fastcall(int, int, int)
+#[doc(alias = "__TIFFsetLongArray")]
+pub fn stub_1827a8() -> ! {
+    todo!("0x1827a8 __TIFFsetLongArray")
+}
+
+// 0x1827b0 — __TIFFsetShortArray
+#[doc(alias = "__TIFFsetShortArray")]
+pub fn stub_1827b0() -> ! {
+    todo!("0x1827b0 __TIFFsetShortArray")
+}
+
+// 0x1827b8 — __TIFFsetNString
+#[doc(alias = "__TIFFsetNString")]
+pub fn stub_1827b8() -> ! {
+    todo!("0x1827b8 __TIFFsetNString")
+}
+
+// 0x1827c0 — __TIFFsetByteArray
+#[doc(alias = "__TIFFsetByteArray")]
+pub fn stub_1827c0() -> ! {
+    todo!("0x1827c0 __TIFFsetByteArray")
+}
+
+// 0x1827c8 — __TIFFsetString
+// type: int __fastcall(int, char *__s)
+#[doc(alias = "__TIFFsetString")]
+pub fn stub_1827c8() -> ! {
+    todo!("0x1827c8 __TIFFsetString")
+}
+
+// 0x1827f8 — __TIFFVSetField
+#[doc(alias = "__TIFFVSetField")]
+pub fn stub_1827f8() -> ! {
+    todo!("0x1827f8 __TIFFVSetField")
+}
+
+// 0x183cd4 — __TIFFGetFieldInfo
+#[doc(alias = "__TIFFGetFieldInfo")]
+pub fn stub_183cd4() -> ! {
+    todo!("0x183cd4 __TIFFGetFieldInfo")
+}
+
+// 0x183cec — __TIFFGetExifFieldInfo
+#[doc(alias = "__TIFFGetExifFieldInfo")]
+pub fn stub_183cec() -> ! {
+    todo!("0x183cec __TIFFGetExifFieldInfo")
+}
+
+// 0x183d04 — _tagCompare
+#[doc(alias = "_tagCompare")]
+pub fn stub_183d04() -> ! {
+    todo!("0x183d04 _tagCompare")
+}
+
+// 0x183d34 — _TIFFDataWidth
+// type: int __fastcall(_DWORD)
+#[doc(alias = "_TIFFDataWidth")]
+pub fn stub_183d34() -> ! {
+    todo!("0x183d34 _TIFFDataWidth")
+}
+
+// 0x183da0 — __TIFFDataSize
+#[doc(alias = "__TIFFDataSize")]
+pub fn stub_183da0() -> ! {
+    todo!("0x183da0 __TIFFDataSize")
+}
+
+// 0x183e0c — __TIFFSampleToTagType
+#[doc(alias = "__TIFFSampleToTagType")]
+pub fn stub_183e0c() -> ! {
+    todo!("0x183e0c __TIFFSampleToTagType")
+}
+
+// 0x183e88 — __TIFFCreateAnonFieldInfo
+#[doc(alias = "__TIFFCreateAnonFieldInfo")]
+pub fn stub_183e88() -> ! {
+    todo!("0x183e88 __TIFFCreateAnonFieldInfo")
+}
+
+// 0x183f24 — _TIFFFindFieldInfo
+#[doc(alias = "_TIFFFindFieldInfo")]
+pub fn stub_183f24() -> ! {
+    todo!("0x183f24 _TIFFFindFieldInfo")
+}
+
+// 0x183fdc — _TIFFFieldWithTag
+// type: int __fastcall(_DWORD, _DWORD)
+#[doc(alias = "_TIFFFieldWithTag")]
+pub fn stub_183fdc() -> ! {
+    todo!("0x183fdc _TIFFFieldWithTag")
+}
+
+// 0x18404c — __TIFFMergeFieldInfo
+#[doc(alias = "__TIFFMergeFieldInfo")]
+pub fn stub_18404c() -> ! {
+    todo!("0x18404c __TIFFMergeFieldInfo")
+}
+
+// 0x184290 — _TIFFMergeFieldInfo
+#[doc(alias = "_TIFFMergeFieldInfo")]
+pub fn stub_184290() -> ! {
+    todo!("0x184290 _TIFFMergeFieldInfo")
+}
+
+// 0x1842d4 — __TIFFSetupFieldInfo
+#[doc(alias = "__TIFFSetupFieldInfo")]
+pub fn stub_1842d4() -> ! {
+    todo!("0x1842d4 __TIFFSetupFieldInfo")
+}
+
+// 0x1843b4 — _TIFFReadDirectoryFind
+#[doc(alias = "_TIFFReadDirectoryFind")]
+pub fn stub_1843b4() -> ! {
+    todo!("0x1843b4 _TIFFReadDirectoryFind")
+}
+
+// 0x1843ec — _TIFFFetchDirectory
+#[doc(alias = "_TIFFFetchDirectory")]
+pub fn stub_1843ec() -> ! {
+    todo!("0x1843ec _TIFFFetchDirectory")
+}
+
+// 0x184708 — _cvtRational
+// type: int __fastcall(int, int, int, int, float *)
+#[doc(alias = "_cvtRational")]
+pub fn stub_184708() -> ! {
+    todo!("0x184708 _cvtRational")
+}
+
+// 0x1847a0 — _CheckDirCount
+#[doc(alias = "_CheckDirCount")]
+pub fn stub_1847a0() -> ! {
+    todo!("0x1847a0 _CheckDirCount")
+}
+
+// 0x18484c — _TIFFFetchData
+#[doc(alias = "_TIFFFetchData")]
+pub fn stub_18484c() -> ! {
+    todo!("0x18484c _TIFFFetchData")
+}
+
+// 0x1849d8 — _TIFFFetchDoubleArray
+#[doc(alias = "_TIFFFetchDoubleArray")]
+pub fn stub_1849d8() -> ! {
+    todo!("0x1849d8 _TIFFFetchDoubleArray")
+}
+
+// 0x1849f0 — _TIFFFetchFloatArray
+#[doc(alias = "_TIFFFetchFloatArray")]
+pub fn stub_1849f0() -> ! {
+    todo!("0x1849f0 _TIFFFetchFloatArray")
+}
+
+// 0x184a28 — _TIFFFetchRationalArray
+#[doc(alias = "_TIFFFetchRationalArray")]
+pub fn stub_184a28() -> ! {
+    todo!("0x184a28 _TIFFFetchRationalArray")
+}
+
+// 0x184af8 — _TIFFFetchLongArray
+#[doc(alias = "_TIFFFetchLongArray")]
+pub fn stub_184af8() -> ! {
+    todo!("0x184af8 _TIFFFetchLongArray")
+}
+
+// 0x184b30 — _TIFFFetchPerSampleLongs
+#[doc(alias = "_TIFFFetchPerSampleLongs")]
+pub fn stub_184b30() -> ! {
+    todo!("0x184b30 _TIFFFetchPerSampleLongs")
+}
+
+// 0x184c60 — _TIFFFetchShortArray
+#[doc(alias = "_TIFFFetchShortArray")]
+pub fn stub_184c60() -> ! {
+    todo!("0x184c60 _TIFFFetchShortArray")
+}
+
+// 0x184cf0 — _TIFFFetchStripThing
+#[doc(alias = "_TIFFFetchStripThing")]
+pub fn stub_184cf0() -> ! {
+    todo!("0x184cf0 _TIFFFetchStripThing")
+}
+
+// 0x1851c8 — _TIFFFetchPerSampleShorts
+#[doc(alias = "_TIFFFetchPerSampleShorts")]
+pub fn stub_1851c8() -> ! {
+    todo!("0x1851c8 _TIFFFetchPerSampleShorts")
+}
+
+// 0x1852fc — _TIFFFetchByteArray
+#[doc(alias = "_TIFFFetchByteArray")]
+pub fn stub_1852fc() -> ! {
+    todo!("0x1852fc _TIFFFetchByteArray")
+}
+
+// 0x1853dc — _TIFFFetchString
+#[doc(alias = "_TIFFFetchString")]
+pub fn stub_1853dc() -> ! {
+    todo!("0x1853dc _TIFFFetchString")
+}
+
+// 0x185440 — _TIFFFetchNormalTag
+#[doc(alias = "_TIFFFetchNormalTag")]
+pub fn stub_185440() -> ! {
+    todo!("0x185440 _TIFFFetchNormalTag")
+}
+
+// 0x185944 — _TIFFReadCustomDirectory
+#[doc(alias = "_TIFFReadCustomDirectory")]
+pub fn stub_185944() -> ! {
+    todo!("0x185944 _TIFFReadCustomDirectory")
+}
+
+// 0x1861f8 — _TIFFReadEXIFDirectory
+#[doc(alias = "_TIFFReadEXIFDirectory")]
+pub fn stub_1861f8() -> ! {
+    todo!("0x1861f8 _TIFFReadEXIFDirectory")
+}
+
+// 0x186230 — _EstimateStripByteCounts
+#[doc(alias = "_EstimateStripByteCounts")]
+pub fn stub_186230() -> ! {
+    todo!("0x186230 _EstimateStripByteCounts")
+}
+
+// 0x186418 — _TIFFReadDirectory
+#[doc(alias = "_TIFFReadDirectory")]
+pub fn stub_186418() -> ! {
+    todo!("0x186418 _TIFFReadDirectory")
+}
+
+// 0x188d70 — _TIFFSetupShortLong
+#[doc(alias = "_TIFFSetupShortLong")]
+pub fn stub_188d70() -> ! {
+    todo!("0x188d70 _TIFFSetupShortLong")
+}
+
+// 0x188dd4 — _TIFFSetupShort
+#[doc(alias = "_TIFFSetupShort")]
+pub fn stub_188dd4() -> ! {
+    todo!("0x188dd4 _TIFFSetupShort")
+}
+
+// 0x188e30 — _TIFFWriteData
+// type: int __fastcall(int, unsigned __int16 *, int)
+#[doc(alias = "_TIFFWriteData")]
+pub fn stub_188e30() -> ! {
+    todo!("0x188e30 _TIFFWriteData")
+}
+
+// 0x188f7c — _TIFFWriteDoubleArray
+#[doc(alias = "_TIFFWriteDoubleArray")]
+pub fn stub_188f7c() -> ! {
+    todo!("0x188f7c _TIFFWriteDoubleArray")
+}
+
+// 0x188f80 — _TIFFWriteFloatArray
+#[doc(alias = "_TIFFWriteFloatArray")]
+pub fn stub_188f80() -> ! {
+    todo!("0x188f80 _TIFFWriteFloatArray")
+}
+
+// 0x188fb0 — _TIFFWriteLongArray
+#[doc(alias = "_TIFFWriteLongArray")]
+pub fn stub_188fb0() -> ! {
+    todo!("0x188fb0 _TIFFWriteLongArray")
+}
+
+// 0x188fe0 — _TIFFWriteShortArray
+#[doc(alias = "_TIFFWriteShortArray")]
+pub fn stub_188fe0() -> ! {
+    todo!("0x188fe0 _TIFFWriteShortArray")
+}
+
+// 0x189060 — _TIFFWriteByteArray
+#[doc(alias = "_TIFFWriteByteArray")]
+pub fn stub_189060() -> ! {
+    todo!("0x189060 _TIFFWriteByteArray")
+}
+
+// 0x189130 — _TIFFWriteShortTable
+#[doc(alias = "_TIFFWriteShortTable")]
+pub fn stub_189130() -> ! {
+    todo!("0x189130 _TIFFWriteShortTable")
+}
+
+// 0x189390 — _TIFFWriteRationalArray
+#[doc(alias = "_TIFFWriteRationalArray")]
+pub fn stub_189390() -> ! {
+    todo!("0x189390 _TIFFWriteRationalArray")
+}
+
+// 0x18952c — _TIFFSetupShortPair
+#[doc(alias = "_TIFFSetupShortPair")]
+pub fn stub_18952c() -> ! {
+    todo!("0x18952c _TIFFSetupShortPair")
+}
+
+// 0x18957c — __TIFFWriteDirectory
+#[doc(alias = "__TIFFWriteDirectory")]
+pub fn stub_18957c() -> ! {
+    todo!("0x18957c __TIFFWriteDirectory")
+}
+
+// 0x18b894 — _TIFFWriteDirectory
+#[doc(alias = "_TIFFWriteDirectory")]
+pub fn stub_18b894() -> ! {
+    todo!("0x18b894 _TIFFWriteDirectory")
+}
+
+// 0x18b89c — _DumpModeSeek
+#[doc(alias = "_DumpModeSeek")]
+pub fn stub_18b89c() -> ! {
+    todo!("0x18b89c _DumpModeSeek")
+}
+
+// 0x18b8c4 — _TIFFInitDumpMode
+#[doc(alias = "_TIFFInitDumpMode")]
+pub fn stub_18b8c4() -> ! {
+    todo!("0x18b8c4 _TIFFInitDumpMode")
+}
+
+// 0x18b90c — _DumpModeEncode
+#[doc(alias = "_DumpModeEncode")]
+pub fn stub_18b90c() -> ! {
+    todo!("0x18b90c _DumpModeEncode")
+}
+
+// 0x18b9e4 — _DumpModeDecode
+#[doc(alias = "_DumpModeDecode")]
+pub fn stub_18b9e4() -> ! {
+    todo!("0x18b9e4 _DumpModeDecode")
+}
+
+// 0x18ba58 — _TIFFErrorExt
+// type: int __fastcall(int, char *)
+#[doc(alias = "_TIFFErrorExt")]
+pub fn stub_18ba58() -> ! {
+    todo!("0x18ba58 _TIFFErrorExt")
+}
+
+// 0x18bad4 — _TIFFGetTagListCount
+#[doc(alias = "_TIFFGetTagListCount")]
+pub fn stub_18bad4() -> ! {
+    todo!("0x18bad4 _TIFFGetTagListCount")
+}
+
+// 0x18badc — _TIFFGetTagListEntry
+#[doc(alias = "_TIFFGetTagListEntry")]
+pub fn stub_18badc() -> ! {
+    todo!("0x18badc _TIFFGetTagListEntry")
+}
+
+// 0x18bb14 — _find0span
+#[doc(alias = "_find0span")]
+pub fn stub_18bb14() -> ! {
+    todo!("0x18bb14 _find0span")
+}
+
+// 0x18bf60 — _find1span
+#[doc(alias = "_find1span")]
+pub fn stub_18bf60() -> ! {
+    todo!("0x18bf60 _find1span")
+}
+
+// 0x18c3ac — _Fax3Extension
+// type: int __fastcall(char *, int)
+#[doc(alias = "_Fax3Extension")]
+pub fn stub_18c3ac() -> ! {
+    todo!("0x18c3ac _Fax3Extension")
+}
+
+// 0x18c424 — _Fax3Unexpected
+// type: int __fastcall(char *)
+#[doc(alias = "_Fax3Unexpected")]
+pub fn stub_18c424() -> ! {
+    todo!("0x18c424 _Fax3Unexpected")
+}
+
+// 0x18c49c — _InitCCITTFax3
+#[doc(alias = "_InitCCITTFax3")]
+pub fn stub_18c49c() -> ! {
+    todo!("0x18c49c _InitCCITTFax3")
+}
+
+// 0x18c668 — _TIFFInitCCITTRLEW
+#[doc(alias = "_TIFFInitCCITTRLEW")]
+pub fn stub_18c668() -> ! {
+    todo!("0x18c668 _TIFFInitCCITTRLEW")
+}
+
+// 0x18c6ac — _TIFFInitCCITTRLE
+#[doc(alias = "_TIFFInitCCITTRLE")]
+pub fn stub_18c6ac() -> ! {
+    todo!("0x18c6ac _TIFFInitCCITTRLE")
+}
+
+// 0x18c6f0 — _TIFFInitCCITTFax4
+#[doc(alias = "_TIFFInitCCITTFax4")]
+pub fn stub_18c6f0() -> ! {
+    todo!("0x18c6f0 _TIFFInitCCITTFax4")
+}
