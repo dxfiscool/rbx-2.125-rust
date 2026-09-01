26287e4728526864d53a42ec11159390bc3484f0
dxf <dxf@local>
2026-09-01 20:30:58 +0200

    script: shard 196 — 100 stubs 0x1fcf00..0x209604 EA-sorted asc global gap filler not yet in script (Script|Lua|Yield|lua 5401/5401 complete, 19452->19552 distinct, rbx_core::SharedPtr not boost, // 0xADDR mangled + doc alias + todo) [skeleton batch]

---
+++ crates/script/src/generated_196.rs
@@ -0,0 +1,614 @@
+// Auto-generated skeletons for rbx-script — global gap filler EA-sorted asc next 100 not yet in script
+// Filter: Script|Lua|Yield|lua (5401 filtered, all already stubbed) — script filler EA-sorted asc
+// Source: ida/export.json (85545 funcs, base 0x4000)
+// Batch: +100 stubs | range 0x1fcf00..0x209604 | script 19452->19552 distinct (filler 0x1fcf00 asc, not-in-script 66093->65993)
+// SharedPtr = rbx_core::SharedPtr (Arc), not boost::shared_ptr; ` and ' stripped from alias
+
+#![allow(non_snake_case, dead_code, unused_variables, unused_imports, clippy::all)]
+
+use rbx_core::SharedPtr;
+
+// 0x1fcf00 — _t1_decoder_done
+#[doc(alias = "_t1_decoder_done")]
+pub fn stub_0x1fcf00() -> ! {
+    todo!("0x1fcf00 _t1_decoder_done")
+}
+
+// 0x1fcf04 — _t1_cmap_std_init
+#[doc(alias = "_t1_cmap_std_init")]
+pub fn stub_0x1fcf04() -> ! {
+    todo!("0x1fcf04 _t1_cmap_std_init")
+}
+
+// 0x1fcf38 — _t1_cmap_std_done
+#[doc(alias = "_t1_cmap_std_done")]
+pub fn stub_0x1fcf38() -> ! {
+    todo!("0x1fcf38 _t1_cmap_std_done")
+}
+
+// 0x1fcf50 — _t1_cmap_standard_init
+#[doc(alias = "_t1_cmap_standard_init")]
+pub fn stub_0x1fcf50() -> ! {
+    todo!("0x1fcf50 _t1_cmap_standard_init")
+}
+
+// 0x1fcf68 — _t1_cmap_expert_init
+#[doc(alias = "_t1_cmap_expert_init")]
+pub fn stub_0x1fcf68() -> ! {
+    todo!("0x1fcf68 _t1_cmap_expert_init")
+}
+
+// 0x1fcf80 — _t1_cmap_custom_init
+#[doc(alias = "_t1_cmap_custom_init")]
+pub fn stub_0x1fcf80() -> ! {
+    todo!("0x1fcf80 _t1_cmap_custom_init")
+}
+
+// 0x1fcfa8 — _t1_cmap_custom_done
+#[doc(alias = "_t1_cmap_custom_done")]
+pub fn stub_0x1fcfa8() -> ! {
+    todo!("0x1fcfa8 _t1_cmap_custom_done")
+}
+
+// 0x1fcfbc — _t1_cmap_custom_char_index
+#[doc(alias = "_t1_cmap_custom_char_index")]
+pub fn stub_0x1fcfbc() -> ! {
+    todo!("0x1fcfbc _t1_cmap_custom_char_index")
+}
+
+// 0x1fcff0 — _t1_cmap_custom_char_next
+#[doc(alias = "_t1_cmap_custom_char_next")]
+pub fn stub_0x1fcff0() -> ! {
+    todo!("0x1fcff0 _t1_cmap_custom_char_next")
+}
+
+// 0x1fd1e0 — _t1_get_glyph_name
+#[doc(alias = "_t1_get_glyph_name")]
+pub fn stub_0x1fd1e0() -> ! {
+    todo!("0x1fd1e0 _t1_get_glyph_name")
+}
+
+// 0x1fd1ec — _t1_cmap_unicode_init
+#[doc(alias = "_t1_cmap_unicode_init")]
+pub fn stub_0x1fd1ec() -> ! {
+    todo!("0x1fd1ec _t1_cmap_unicode_init")
+}
+
+// 0x1fd234 — _t1_cmap_unicode_char_index
+#[doc(alias = "_t1_cmap_unicode_char_index")]
+pub fn stub_0x1fd234() -> ! {
+    todo!("0x1fd234 _t1_cmap_unicode_char_index")
+}
+
+// 0x1fd244 — _t1_cmap_unicode_char_next
+#[doc(alias = "_t1_cmap_unicode_char_next")]
+pub fn stub_0x1fd244() -> ! {
+    todo!("0x1fd244 _t1_cmap_unicode_char_next")
+}
+
+// 0x1fd254 — _afm_stream_skip_spaces
+#[doc(alias = "_afm_stream_skip_spaces")]
+pub fn stub_0x1fd254() -> ! {
+    todo!("0x1fd254 _afm_stream_skip_spaces")
+}
+
+// 0x1fd2c4 — _afm_stream_read_one
+#[doc(alias = "_afm_stream_read_one")]
+pub fn stub_0x1fd2c4() -> ! {
+    todo!("0x1fd2c4 _afm_stream_read_one")
+}
+
+// 0x1fd348 — _afm_stream_read_string
+#[doc(alias = "_afm_stream_read_string")]
+pub fn stub_0x1fd348() -> ! {
+    todo!("0x1fd348 _afm_stream_read_string")
+}
+
+// 0x1fd3b0 — _afm_parser_next_key
+#[doc(alias = "_afm_parser_next_key")]
+pub fn stub_0x1fd3b0() -> ! {
+    todo!("0x1fd3b0 _afm_parser_next_key")
+}
+
+// 0x1fd46c — _afm_compare_kern_pairs
+#[doc(alias = "_afm_compare_kern_pairs")]
+pub fn stub_0x1fd46c() -> ! {
+    todo!("0x1fd46c _afm_compare_kern_pairs")
+}
+
+// 0x1fd49c — _PS_Conv_Strtol
+#[doc(alias = "_PS_Conv_Strtol")]
+pub fn stub_0x1fd49c() -> ! {
+    todo!("0x1fd49c _PS_Conv_Strtol")
+}
+
+// 0x1fd57c — _PS_Conv_ToInt
+#[doc(alias = "_PS_Conv_ToInt")]
+pub fn stub_0x1fd57c() -> ! {
+    todo!("0x1fd57c _PS_Conv_ToInt")
+}
+
+// 0x1fd5d0 — _ps_parser_to_int
+#[doc(alias = "_ps_parser_to_int")]
+pub fn stub_0x1fd5d0() -> ! {
+    todo!("0x1fd5d0 _ps_parser_to_int")
+}
+
+// 0x1fd5f0 — _ps_parser_to_bytes
+#[doc(alias = "_ps_parser_to_bytes")]
+pub fn stub_0x1fd5f0() -> ! {
+    todo!("0x1fd5f0 _ps_parser_to_bytes")
+}
+
+// 0x1fd720 — _t1_decrypt
+#[doc(alias = "_t1_decrypt")]
+pub fn stub_0x1fd720() -> ! {
+    todo!("0x1fd720 _t1_decrypt")
+}
+
+// 0x1fd86c — _afm_parser_done
+#[doc(alias = "_afm_parser_done")]
+pub fn stub_0x1fd86c() -> ! {
+    todo!("0x1fd86c _afm_parser_done")
+}
+
+// 0x1fd88c — _t1_cmap_unicode_done
+#[doc(alias = "_t1_cmap_unicode_done")]
+pub fn stub_0x1fd88c() -> ! {
+    todo!("0x1fd88c _t1_cmap_unicode_done")
+}
+
+// 0x1fd8b8 — _ps_table_release
+#[doc(alias = "_ps_table_release")]
+pub fn stub_0x1fd8b8() -> ! {
+    todo!("0x1fd8b8 _ps_table_release")
+}
+
+// 0x1fd918 — _t1_cmap_std_char_index
+#[doc(alias = "_t1_cmap_std_char_index")]
+pub fn stub_0x1fd918() -> ! {
+    todo!("0x1fd918 _t1_cmap_std_char_index")
+}
+
+// 0x1fdcc0 — _t1_cmap_std_char_next
+#[doc(alias = "_t1_cmap_std_char_next")]
+pub fn stub_0x1fdcc0() -> ! {
+    todo!("0x1fdcc0 _t1_cmap_std_char_next")
+}
+
+// 0x1fdeb8 — _t1_lookup_glyph_by_stdcharcode
+#[doc(alias = "_t1_lookup_glyph_by_stdcharcode")]
+pub fn stub_0x1fdeb8() -> ! {
+    todo!("0x1fdeb8 _t1_lookup_glyph_by_stdcharcode")
+}
+
+// 0x1fe264 — _afm_tokenize
+// type: int __fastcall(char *__s2, size_t __n)
+#[doc(alias = "_afm_tokenize")]
+pub fn stub_0x1fe264() -> ! {
+    todo!("0x1fe264 _afm_tokenize")
+}
+
+// 0x1fe458 — _PS_Conv_ToFixed
+#[doc(alias = "_PS_Conv_ToFixed")]
+pub fn stub_0x1fe458() -> ! {
+    todo!("0x1fe458 _PS_Conv_ToFixed")
+}
+
+// 0x1fea18 — _afm_parser_read_vals
+#[doc(alias = "_afm_parser_read_vals")]
+pub fn stub_0x1fea18() -> ! {
+    todo!("0x1fea18 _afm_parser_read_vals")
+}
+
+// 0x1feb8c — _afm_parser_read_int
+#[doc(alias = "_afm_parser_read_int")]
+pub fn stub_0x1feb8c() -> ! {
+    todo!("0x1feb8c _afm_parser_read_int")
+}
+
+// 0x1febcc — _ps_parser_to_fixed
+#[doc(alias = "_ps_parser_to_fixed")]
+pub fn stub_0x1febcc() -> ! {
+    todo!("0x1febcc _ps_parser_to_fixed")
+}
+
+// 0x1febf4 — _ps_tofixedarray
+#[doc(alias = "_ps_tofixedarray")]
+pub fn stub_0x1febf4() -> ! {
+    todo!("0x1febf4 _ps_tofixedarray")
+}
+
+// 0x1fed14 — _ps_parser_to_fixed_array
+#[doc(alias = "_ps_parser_to_fixed_array")]
+pub fn stub_0x1fed14() -> ! {
+    todo!("0x1fed14 _ps_parser_to_fixed_array")
+}
+
+// 0x1fed5c — _ps_parser_to_coord_array
+#[doc(alias = "_ps_parser_to_coord_array")]
+pub fn stub_0x1fed5c() -> ! {
+    todo!("0x1fed5c _ps_parser_to_coord_array")
+}
+
+// 0x1fee88 — _ps_table_new
+#[doc(alias = "_ps_table_new")]
+pub fn stub_0x1fee88() -> ! {
+    todo!("0x1fee88 _ps_table_new")
+}
+
+// 0x1fef60 — _afm_parser_parse
+#[doc(alias = "_afm_parser_parse")]
+pub fn stub_0x1fef60() -> ! {
+    todo!("0x1fef60 _afm_parser_parse")
+}
+
+// 0x1ff8d8 — _afm_parser_init
+#[doc(alias = "_afm_parser_init")]
+pub fn stub_0x1ff8d8() -> ! {
+    todo!("0x1ff8d8 _afm_parser_init")
+}
+
+// 0x1ff944 — _ps_table_done
+#[doc(alias = "_ps_table_done")]
+pub fn stub_0x1ff944() -> ! {
+    todo!("0x1ff944 _ps_table_done")
+}
+
+// 0x1ff9b8 — _t1_builder_add_point
+#[doc(alias = "_t1_builder_add_point")]
+pub fn stub_0x1ff9b8() -> ! {
+    todo!("0x1ff9b8 _t1_builder_add_point")
+}
+
+// 0x1ffa4c — _ps_parser_load_field
+#[doc(alias = "_ps_parser_load_field")]
+pub fn stub_0x1ffa4c() -> ! {
+    todo!("0x1ffa4c _ps_parser_load_field")
+}
+
+// 0x1ffd6c — _ps_parser_load_field_table
+#[doc(alias = "_ps_parser_load_field_table")]
+pub fn stub_0x1ffd6c() -> ! {
+    todo!("0x1ffd6c _ps_parser_load_field_table")
+}
+
+// 0x1ffea0 — _t1_builder_add_contour
+#[doc(alias = "_t1_builder_add_contour")]
+pub fn stub_0x1ffea0() -> ! {
+    todo!("0x1ffea0 _t1_builder_add_contour")
+}
+
+// 0x1fff48 — _t1_builder_check_points
+#[doc(alias = "_t1_builder_check_points")]
+pub fn stub_0x1fff48() -> ! {
+    todo!("0x1fff48 _t1_builder_check_points")
+}
+
+// 0x1fff8c — _t1_builder_add_point1
+#[doc(alias = "_t1_builder_add_point1")]
+pub fn stub_0x1fff8c() -> ! {
+    todo!("0x1fff8c _t1_builder_add_point1")
+}
+
+// 0x1fffd4 — _t1_builder_start_point
+#[doc(alias = "_t1_builder_start_point")]
+pub fn stub_0x1fffd4() -> ! {
+    todo!("0x1fffd4 _t1_builder_start_point")
+}
+
+// 0x200020 — _t1_builder_init
+#[doc(alias = "_t1_builder_init")]
+pub fn stub_0x200020() -> ! {
+    todo!("0x200020 _t1_builder_init")
+}
+
+// 0x2000e8 — _ps_table_add
+#[doc(alias = "_ps_table_add")]
+pub fn stub_0x2000e8() -> ! {
+    todo!("0x2000e8 _ps_table_add")
+}
+
+// 0x20022c — _t1_decoder_parse_charstrings
+#[doc(alias = "_t1_decoder_parse_charstrings")]
+pub fn stub_0x20022c() -> ! {
+    todo!("0x20022c _t1_decoder_parse_charstrings")
+}
+
+// 0x20157c — _t1_decoder_init
+#[doc(alias = "_t1_decoder_init")]
+pub fn stub_0x20157c() -> ! {
+    todo!("0x20157c _t1_decoder_init")
+}
+
+// 0x20163c — _ps_mask_test_bit
+#[doc(alias = "_ps_mask_test_bit")]
+pub fn stub_0x20163c() -> ! {
+    todo!("0x20163c _ps_mask_test_bit")
+}
+
+// 0x201660 — _ps_dimension_end_mask
+#[doc(alias = "_ps_dimension_end_mask")]
+pub fn stub_0x201660() -> ! {
+    todo!("0x201660 _ps_dimension_end_mask")
+}
+
+// 0x20167c — _ps_hints_open
+// type: int __fastcall(_DWORD, _DWORD)
+#[doc(alias = "_ps_hints_open")]
+pub fn stub_0x20167c() -> ! {
+    todo!("0x20167c _ps_hints_open")
+}
+
+// 0x2016bc — _t1_hints_open
+#[doc(alias = "_t1_hints_open")]
+pub fn stub_0x2016bc() -> ! {
+    todo!("0x2016bc _t1_hints_open")
+}
+
+// 0x2016c4 — _t2_hints_open
+// type: int __fastcall(int)
+#[doc(alias = "_t2_hints_open")]
+pub fn stub_0x2016c4() -> ! {
+    todo!("0x2016c4 _t2_hints_open")
+}
+
+// 0x2016cc — _psh_blues_set_zones_0
+#[doc(alias = "_psh_blues_set_zones_0")]
+pub fn stub_0x2016cc() -> ! {
+    todo!("0x2016cc _psh_blues_set_zones_0")
+}
+
+// 0x201c18 — _psh_blues_set_zones
+#[doc(alias = "_psh_blues_set_zones")]
+pub fn stub_0x201c18() -> ! {
+    todo!("0x201c18 _psh_blues_set_zones")
+}
+
+// 0x202294 — _psh_hint_table_record
+#[doc(alias = "_psh_hint_table_record")]
+pub fn stub_0x202294() -> ! {
+    todo!("0x202294 _psh_hint_table_record")
+}
+
+// 0x202460 — _psh_hint_table_activate_mask
+#[doc(alias = "_psh_hint_table_activate_mask")]
+pub fn stub_0x202460() -> ! {
+    todo!("0x202460 _psh_hint_table_activate_mask")
+}
+
+// 0x2029cc — _psh_compute_dir
+#[doc(alias = "_psh_compute_dir")]
+pub fn stub_0x2029cc() -> ! {
+    todo!("0x2029cc _psh_compute_dir")
+}
+
+// 0x202a30 — _psh_glyph_load_points
+#[doc(alias = "_psh_glyph_load_points")]
+pub fn stub_0x202a30() -> ! {
+    todo!("0x202a30 _psh_glyph_load_points")
+}
+
+// 0x202bf4 — _psh_hint_table_find_strong_points
+#[doc(alias = "_psh_hint_table_find_strong_points")]
+pub fn stub_0x202bf4() -> ! {
+    todo!("0x202bf4 _psh_hint_table_find_strong_points")
+}
+
+// 0x20370c — _pshinter_get_globals_funcs
+#[doc(alias = "_pshinter_get_globals_funcs")]
+pub fn stub_0x20370c() -> ! {
+    todo!("0x20370c _pshinter_get_globals_funcs")
+}
+
+// 0x203714 — _pshinter_get_t1_funcs
+#[doc(alias = "_pshinter_get_t1_funcs")]
+pub fn stub_0x203714() -> ! {
+    todo!("0x203714 _pshinter_get_t1_funcs")
+}
+
+// 0x20371c — _pshinter_get_t2_funcs
+#[doc(alias = "_pshinter_get_t2_funcs")]
+pub fn stub_0x20371c() -> ! {
+    todo!("0x20371c _pshinter_get_t2_funcs")
+}
+
+// 0x203724 — _psh_hint_table_done
+#[doc(alias = "_psh_hint_table_done")]
+pub fn stub_0x203724() -> ! {
+    todo!("0x203724 _psh_hint_table_done")
+}
+
+// 0x203780 — _psh_globals_destroy
+#[doc(alias = "_psh_globals_destroy")]
+pub fn stub_0x203780() -> ! {
+    todo!("0x203780 _psh_globals_destroy")
+}
+
+// 0x2037b8 — _ps_mask_table_done
+#[doc(alias = "_ps_mask_table_done")]
+pub fn stub_0x2037b8() -> ! {
+    todo!("0x2037b8 _ps_mask_table_done")
+}
+
+// 0x203924 — _ps_dimension_done
+#[doc(alias = "_ps_dimension_done")]
+pub fn stub_0x203924() -> ! {
+    todo!("0x203924 _ps_dimension_done")
+}
+
+// 0x203968 — _ps_hinter_done
+#[doc(alias = "_ps_hinter_done")]
+pub fn stub_0x203968() -> ! {
+    todo!("0x203968 _ps_hinter_done")
+}
+
+// 0x2039a8 — _ps_hinter_init
+#[doc(alias = "_ps_hinter_init")]
+pub fn stub_0x2039a8() -> ! {
+    todo!("0x2039a8 _ps_hinter_init")
+}
+
+// 0x203ab8 — _psh_hint_align
+#[doc(alias = "_psh_hint_align")]
+pub fn stub_0x203ab8() -> ! {
+    todo!("0x203ab8 _psh_hint_align")
+}
+
+// 0x2041bc — _psh_globals_scale_widths
+#[doc(alias = "_psh_globals_scale_widths")]
+pub fn stub_0x2041bc() -> ! {
+    todo!("0x2041bc _psh_globals_scale_widths")
+}
+
+// 0x204418 — _psh_globals_set_scale
+#[doc(alias = "_psh_globals_set_scale")]
+pub fn stub_0x204418() -> ! {
+    todo!("0x204418 _psh_globals_set_scale")
+}
+
+// 0x204a00 — _psh_globals_new
+#[doc(alias = "_psh_globals_new")]
+pub fn stub_0x204a00() -> ! {
+    todo!("0x204a00 _psh_globals_new")
+}
+
+// 0x204d28 — _psh_hint_table_init
+#[doc(alias = "_psh_hint_table_init")]
+pub fn stub_0x204d28() -> ! {
+    todo!("0x204d28 _psh_hint_table_init")
+}
+
+// 0x205430 — _ps_mask_table_alloc
+#[doc(alias = "_ps_mask_table_alloc")]
+pub fn stub_0x205430() -> ! {
+    todo!("0x205430 _ps_mask_table_alloc")
+}
+
+// 0x2054d0 — _ps_dimension_reset_mask
+#[doc(alias = "_ps_dimension_reset_mask")]
+pub fn stub_0x2054d0() -> ! {
+    todo!("0x2054d0 _ps_dimension_reset_mask")
+}
+
+// 0x205500 — _ps_hints_t1reset
+#[doc(alias = "_ps_hints_t1reset")]
+pub fn stub_0x205500() -> ! {
+    todo!("0x205500 _ps_hints_t1reset")
+}
+
+// 0x205564 — _ps_mask_table_last
+#[doc(alias = "_ps_mask_table_last")]
+pub fn stub_0x205564() -> ! {
+    todo!("0x205564 _ps_mask_table_last")
+}
+
+// 0x2055b4 — _ps_mask_ensure
+// type: int __fastcall(int, int, int)
+#[doc(alias = "_ps_mask_ensure")]
+pub fn stub_0x2055b4() -> ! {
+    todo!("0x2055b4 _ps_mask_ensure")
+}
+
+// 0x205630 — _ps_dimension_set_mask_bits
+#[doc(alias = "_ps_dimension_set_mask_bits")]
+pub fn stub_0x205630() -> ! {
+    todo!("0x205630 _ps_dimension_set_mask_bits")
+}
+
+// 0x205868 — _ps_hints_t2counter
+#[doc(alias = "_ps_hints_t2counter")]
+pub fn stub_0x205868() -> ! {
+    todo!("0x205868 _ps_hints_t2counter")
+}
+
+// 0x2058f4 — _ps_hints_t2mask
+#[doc(alias = "_ps_hints_t2mask")]
+pub fn stub_0x2058f4() -> ! {
+    todo!("0x2058f4 _ps_hints_t2mask")
+}
+
+// 0x20597c — _ps_mask_set_bit
+#[doc(alias = "_ps_mask_set_bit")]
+pub fn stub_0x20597c() -> ! {
+    todo!("0x20597c _ps_mask_set_bit")
+}
+
+// 0x2059d8 — _ps_dimension_add_t1stem
+#[doc(alias = "_ps_dimension_add_t1stem")]
+pub fn stub_0x2059d8() -> ! {
+    todo!("0x2059d8 _ps_dimension_add_t1stem")
+}
+
+// 0x205d40 — _ps_hints_stem
+#[doc(alias = "_ps_hints_stem")]
+pub fn stub_0x205d40() -> ! {
+    todo!("0x205d40 _ps_hints_stem")
+}
+
+// 0x205ed8 — _t2_hints_stems
+#[doc(alias = "_t2_hints_stems")]
+pub fn stub_0x205ed8() -> ! {
+    todo!("0x205ed8 _t2_hints_stems")
+}
+
+// 0x206268 — _t1_hints_stem
+#[doc(alias = "_t1_hints_stem")]
+pub fn stub_0x206268() -> ! {
+    todo!("0x206268 _t1_hints_stem")
+}
+
+// 0x2062bc — _ps_hints_t1stem3
+#[doc(alias = "_ps_hints_t1stem3")]
+pub fn stub_0x2062bc() -> ! {
+    todo!("0x2062bc _ps_hints_t1stem3")
+}
+
+// 0x206560 — _ps_dimension_end
+#[doc(alias = "_ps_dimension_end")]
+pub fn stub_0x206560() -> ! {
+    todo!("0x206560 _ps_dimension_end")
+}
+
+// 0x206cd0 — _ps_hints_close
+#[doc(alias = "_ps_hints_close")]
+pub fn stub_0x206cd0() -> ! {
+    todo!("0x206cd0 _ps_hints_close")
+}
+
+// 0x206d18 — _ps_hints_apply
+#[doc(alias = "_ps_hints_apply")]
+pub fn stub_0x206d18() -> ! {
+    todo!("0x206d18 _ps_hints_apply")
+}
+
+// 0x2091c0 — _ft_get_adobe_glyph_index
+#[doc(alias = "_ft_get_adobe_glyph_index")]
+pub fn stub_0x2091c0() -> ! {
+    todo!("0x2091c0 _ft_get_adobe_glyph_index")
+}
+
+// 0x20930c — _ps_unicode_value
+// type: int __fastcall(_DWORD)
+#[doc(alias = "_ps_unicode_value")]
+pub fn stub_0x20930c() -> ! {
+    todo!("0x20930c _ps_unicode_value")
+}
+
+// 0x209558 — _compare_uni_maps
+#[doc(alias = "_compare_uni_maps")]
+pub fn stub_0x209558() -> ! {
+    todo!("0x209558 _compare_uni_maps")
+}
+
+// 0x20958c — _ps_unicodes_char_index
+#[doc(alias = "_ps_unicodes_char_index")]
+pub fn stub_0x20958c() -> ! {
+    todo!("0x20958c _ps_unicodes_char_index")
+}
+
+// 0x209604 — _ps_unicodes_char_next
+#[doc(alias = "_ps_unicodes_char_next")]
+pub fn stub_0x209604() -> ! {
+    todo!("0x209604 _ps_unicodes_char_next")
+}
