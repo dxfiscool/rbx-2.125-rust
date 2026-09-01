b0c54a91a7d3b4002247078d21b5d1802bf39d7f
dxf <dxf@local>
2026-09-01 08:36:36 +0200

    audio: fix lib.rs mod generated_123 export

--- crates/audio/src/lib.rs
+++ crates/audio/src/lib.rs
@@ -123,6 +123,7 @@ pub mod generated_119;
 pub mod generated_120;
 pub mod generated_121;
 pub mod generated_122;
+pub mod generated_123;
 // 0x686a4 — __ZN4FMOD10ProfileCpu4initEv
 #[doc(alias = "FMOD::ProfileCpu::init(void)")]
 pub fn stub_686a4() -> ! {
