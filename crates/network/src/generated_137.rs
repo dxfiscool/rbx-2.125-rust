38152bbd96d9544c8a7451cd7fe4d197b8fce155
dxf <dxf@local>
2026-09-01 09:04:59 +0200

    network: generated_137 — 100 stubs 0xf6f4c8..0xf6f96c EA-sorted asc filler (RBX::Network 4797 filtered done, 16129->16229, rbx_core::SharedPtr not boost) [skeleton batch]

---
+++ crates/network/src/generated_137.rs
@@ -0,0 +1,608 @@
+//! Auto-generated skeletons for rbx-network — filler global ascending EA-sorted
+//! Filter: RakNet|RBX::Network|Replicator (case-insensitive) -> 4797 funcs, 4797 already stubbed (0 remaining before batch); filler global ascending
+//! Source: ida/export.json (85545 funcs, base 0x4000)
+//! Batch: +100 stubs | range 0xf6f4c8..0xf6f96c | existing 16129 -> 16229 total (filler global ascending EA-sorted, rbx_core::SharedPtr not boost)
+
+#![allow(non_snake_case, dead_code, unused_variables, unused_imports, clippy::all)]
+
+use rbx_core::SharedPtr;
+
+// 0xf6f4c8 — sub_F6F4C8
+#[doc(alias = "sub_F6F4C8")]
+pub fn stub_f6f4c8() -> ! {
+    todo!("0xf6f4c8 sub_F6F4C8")
+}
+
+// 0xf6f4d4 — sub_F6F4D4
+#[doc(alias = "sub_F6F4D4")]
+pub fn stub_f6f4d4() -> ! {
+    todo!("0xf6f4d4 sub_F6F4D4")
+}
+
+// 0xf6f4e0 — sub_F6F4E0
+#[doc(alias = "sub_F6F4E0")]
+pub fn stub_f6f4e0() -> ! {
+    todo!("0xf6f4e0 sub_F6F4E0")
+}
+
+// 0xf6f4ec — sub_F6F4EC
+#[doc(alias = "sub_F6F4EC")]
+pub fn stub_f6f4ec() -> ! {
+    todo!("0xf6f4ec sub_F6F4EC")
+}
+
+// 0xf6f4f8 — sub_F6F4F8
+#[doc(alias = "sub_F6F4F8")]
+pub fn stub_f6f4f8() -> ! {
+    todo!("0xf6f4f8 sub_F6F4F8")
+}
+
+// 0xf6f504 — sub_F6F504
+#[doc(alias = "sub_F6F504")]
+pub fn stub_f6f504() -> ! {
+    todo!("0xf6f504 sub_F6F504")
+}
+
+// 0xf6f510 — sub_F6F510
+#[doc(alias = "sub_F6F510")]
+pub fn stub_f6f510() -> ! {
+    todo!("0xf6f510 sub_F6F510")
+}
+
+// 0xf6f51c — sub_F6F51C
+#[doc(alias = "sub_F6F51C")]
+pub fn stub_f6f51c() -> ! {
+    todo!("0xf6f51c sub_F6F51C")
+}
+
+// 0xf6f528 — sub_F6F528
+#[doc(alias = "sub_F6F528")]
+pub fn stub_f6f528() -> ! {
+    todo!("0xf6f528 sub_F6F528")
+}
+
+// 0xf6f534 — sub_F6F534
+#[doc(alias = "sub_F6F534")]
+pub fn stub_f6f534() -> ! {
+    todo!("0xf6f534 sub_F6F534")
+}
+
+// 0xf6f540 — sub_F6F540
+#[doc(alias = "sub_F6F540")]
+pub fn stub_f6f540() -> ! {
+    todo!("0xf6f540 sub_F6F540")
+}
+
+// 0xf6f54c — sub_F6F54C
+#[doc(alias = "sub_F6F54C")]
+pub fn stub_f6f54c() -> ! {
+    todo!("0xf6f54c sub_F6F54C")
+}
+
+// 0xf6f558 — sub_F6F558
+#[doc(alias = "sub_F6F558")]
+pub fn stub_f6f558() -> ! {
+    todo!("0xf6f558 sub_F6F558")
+}
+
+// 0xf6f564 — sub_F6F564
+#[doc(alias = "sub_F6F564")]
+pub fn stub_f6f564() -> ! {
+    todo!("0xf6f564 sub_F6F564")
+}
+
+// 0xf6f570 — sub_F6F570
+#[doc(alias = "sub_F6F570")]
+pub fn stub_f6f570() -> ! {
+    todo!("0xf6f570 sub_F6F570")
+}
+
+// 0xf6f57c — sub_F6F57C
+#[doc(alias = "sub_F6F57C")]
+pub fn stub_f6f57c() -> ! {
+    todo!("0xf6f57c sub_F6F57C")
+}
+
+// 0xf6f588 — sub_F6F588
+#[doc(alias = "sub_F6F588")]
+pub fn stub_f6f588() -> ! {
+    todo!("0xf6f588 sub_F6F588")
+}
+
+// 0xf6f594 — sub_F6F594
+#[doc(alias = "sub_F6F594")]
+pub fn stub_f6f594() -> ! {
+    todo!("0xf6f594 sub_F6F594")
+}
+
+// 0xf6f5a0 — sub_F6F5A0
+#[doc(alias = "sub_F6F5A0")]
+pub fn stub_f6f5a0() -> ! {
+    todo!("0xf6f5a0 sub_F6F5A0")
+}
+
+// 0xf6f5ac — sub_F6F5AC
+#[doc(alias = "sub_F6F5AC")]
+pub fn stub_f6f5ac() -> ! {
+    todo!("0xf6f5ac sub_F6F5AC")
+}
+
+// 0xf6f5b8 — sub_F6F5B8
+#[doc(alias = "sub_F6F5B8")]
+pub fn stub_f6f5b8() -> ! {
+    todo!("0xf6f5b8 sub_F6F5B8")
+}
+
+// 0xf6f5c4 — sub_F6F5C4
+#[doc(alias = "sub_F6F5C4")]
+pub fn stub_f6f5c4() -> ! {
+    todo!("0xf6f5c4 sub_F6F5C4")
+}
+
+// 0xf6f5d0 — sub_F6F5D0
+#[doc(alias = "sub_F6F5D0")]
+pub fn stub_f6f5d0() -> ! {
+    todo!("0xf6f5d0 sub_F6F5D0")
+}
+
+// 0xf6f5dc — sub_F6F5DC
+#[doc(alias = "sub_F6F5DC")]
+pub fn stub_f6f5dc() -> ! {
+    todo!("0xf6f5dc sub_F6F5DC")
+}
+
+// 0xf6f5e8 — sub_F6F5E8
+#[doc(alias = "sub_F6F5E8")]
+pub fn stub_f6f5e8() -> ! {
+    todo!("0xf6f5e8 sub_F6F5E8")
+}
+
+// 0xf6f5f4 — sub_F6F5F4
+#[doc(alias = "sub_F6F5F4")]
+pub fn stub_f6f5f4() -> ! {
+    todo!("0xf6f5f4 sub_F6F5F4")
+}
+
+// 0xf6f600 — sub_F6F600
+#[doc(alias = "sub_F6F600")]
+pub fn stub_f6f600() -> ! {
+    todo!("0xf6f600 sub_F6F600")
+}
+
+// 0xf6f60c — sub_F6F60C
+#[doc(alias = "sub_F6F60C")]
+pub fn stub_f6f60c() -> ! {
+    todo!("0xf6f60c sub_F6F60C")
+}
+
+// 0xf6f618 — sub_F6F618
+#[doc(alias = "sub_F6F618")]
+pub fn stub_f6f618() -> ! {
+    todo!("0xf6f618 sub_F6F618")
+}
+
+// 0xf6f624 — sub_F6F624
+#[doc(alias = "sub_F6F624")]
+pub fn stub_f6f624() -> ! {
+    todo!("0xf6f624 sub_F6F624")
+}
+
+// 0xf6f630 — sub_F6F630
+#[doc(alias = "sub_F6F630")]
+pub fn stub_f6f630() -> ! {
+    todo!("0xf6f630 sub_F6F630")
+}
+
+// 0xf6f63c — sub_F6F63C
+#[doc(alias = "sub_F6F63C")]
+pub fn stub_f6f63c() -> ! {
+    todo!("0xf6f63c sub_F6F63C")
+}
+
+// 0xf6f648 — sub_F6F648
+#[doc(alias = "sub_F6F648")]
+pub fn stub_f6f648() -> ! {
+    todo!("0xf6f648 sub_F6F648")
+}
+
+// 0xf6f654 — sub_F6F654
+#[doc(alias = "sub_F6F654")]
+pub fn stub_f6f654() -> ! {
+    todo!("0xf6f654 sub_F6F654")
+}
+
+// 0xf6f660 — sub_F6F660
+#[doc(alias = "sub_F6F660")]
+pub fn stub_f6f660() -> ! {
+    todo!("0xf6f660 sub_F6F660")
+}
+
+// 0xf6f66c — sub_F6F66C
+#[doc(alias = "sub_F6F66C")]
+pub fn stub_f6f66c() -> ! {
+    todo!("0xf6f66c sub_F6F66C")
+}
+
+// 0xf6f678 — sub_F6F678
+#[doc(alias = "sub_F6F678")]
+pub fn stub_f6f678() -> ! {
+    todo!("0xf6f678 sub_F6F678")
+}
+
+// 0xf6f684 — sub_F6F684
+#[doc(alias = "sub_F6F684")]
+pub fn stub_f6f684() -> ! {
+    todo!("0xf6f684 sub_F6F684")
+}
+
+// 0xf6f690 — sub_F6F690
+#[doc(alias = "sub_F6F690")]
+pub fn stub_f6f690() -> ! {
+    todo!("0xf6f690 sub_F6F690")
+}
+
+// 0xf6f69c — sub_F6F69C
+#[doc(alias = "sub_F6F69C")]
+pub fn stub_f6f69c() -> ! {
+    todo!("0xf6f69c sub_F6F69C")
+}
+
+// 0xf6f6a8 — sub_F6F6A8
+#[doc(alias = "sub_F6F6A8")]
+pub fn stub_f6f6a8() -> ! {
+    todo!("0xf6f6a8 sub_F6F6A8")
+}
+
+// 0xf6f6b4 — sub_F6F6B4
+#[doc(alias = "sub_F6F6B4")]
+pub fn stub_f6f6b4() -> ! {
+    todo!("0xf6f6b4 sub_F6F6B4")
+}
+
+// 0xf6f6c0 — sub_F6F6C0
+#[doc(alias = "sub_F6F6C0")]
+pub fn stub_f6f6c0() -> ! {
+    todo!("0xf6f6c0 sub_F6F6C0")
+}
+
+// 0xf6f6cc — sub_F6F6CC
+#[doc(alias = "sub_F6F6CC")]
+pub fn stub_f6f6cc() -> ! {
+    todo!("0xf6f6cc sub_F6F6CC")
+}
+
+// 0xf6f6d8 — sub_F6F6D8
+#[doc(alias = "sub_F6F6D8")]
+pub fn stub_f6f6d8() -> ! {
+    todo!("0xf6f6d8 sub_F6F6D8")
+}
+
+// 0xf6f6e4 — sub_F6F6E4
+#[doc(alias = "sub_F6F6E4")]
+pub fn stub_f6f6e4() -> ! {
+    todo!("0xf6f6e4 sub_F6F6E4")
+}
+
+// 0xf6f6f0 — sub_F6F6F0
+#[doc(alias = "sub_F6F6F0")]
+pub fn stub_f6f6f0() -> ! {
+    todo!("0xf6f6f0 sub_F6F6F0")
+}
+
+// 0xf6f6fc — sub_F6F6FC
+#[doc(alias = "sub_F6F6FC")]
+pub fn stub_f6f6fc() -> ! {
+    todo!("0xf6f6fc sub_F6F6FC")
+}
+
+// 0xf6f708 — sub_F6F708
+#[doc(alias = "sub_F6F708")]
+pub fn stub_f6f708() -> ! {
+    todo!("0xf6f708 sub_F6F708")
+}
+
+// 0xf6f714 — sub_F6F714
+#[doc(alias = "sub_F6F714")]
+pub fn stub_f6f714() -> ! {
+    todo!("0xf6f714 sub_F6F714")
+}
+
+// 0xf6f720 — sub_F6F720
+#[doc(alias = "sub_F6F720")]
+pub fn stub_f6f720() -> ! {
+    todo!("0xf6f720 sub_F6F720")
+}
+
+// 0xf6f72c — sub_F6F72C
+#[doc(alias = "sub_F6F72C")]
+pub fn stub_f6f72c() -> ! {
+    todo!("0xf6f72c sub_F6F72C")
+}
+
+// 0xf6f738 — sub_F6F738
+#[doc(alias = "sub_F6F738")]
+pub fn stub_f6f738() -> ! {
+    todo!("0xf6f738 sub_F6F738")
+}
+
+// 0xf6f744 — sub_F6F744
+#[doc(alias = "sub_F6F744")]
+pub fn stub_f6f744() -> ! {
+    todo!("0xf6f744 sub_F6F744")
+}
+
+// 0xf6f750 — sub_F6F750
+#[doc(alias = "sub_F6F750")]
+pub fn stub_f6f750() -> ! {
+    todo!("0xf6f750 sub_F6F750")
+}
+
+// 0xf6f75c — sub_F6F75C
+#[doc(alias = "sub_F6F75C")]
+pub fn stub_f6f75c() -> ! {
+    todo!("0xf6f75c sub_F6F75C")
+}
+
+// 0xf6f768 — sub_F6F768
+#[doc(alias = "sub_F6F768")]
+pub fn stub_f6f768() -> ! {
+    todo!("0xf6f768 sub_F6F768")
+}
+
+// 0xf6f774 — sub_F6F774
+#[doc(alias = "sub_F6F774")]
+pub fn stub_f6f774() -> ! {
+    todo!("0xf6f774 sub_F6F774")
+}
+
+// 0xf6f780 — sub_F6F780
+#[doc(alias = "sub_F6F780")]
+pub fn stub_f6f780() -> ! {
+    todo!("0xf6f780 sub_F6F780")
+}
+
+// 0xf6f78c — sub_F6F78C
+#[doc(alias = "sub_F6F78C")]
+pub fn stub_f6f78c() -> ! {
+    todo!("0xf6f78c sub_F6F78C")
+}
+
+// 0xf6f798 — sub_F6F798
+#[doc(alias = "sub_F6F798")]
+pub fn stub_f6f798() -> ! {
+    todo!("0xf6f798 sub_F6F798")
+}
+
+// 0xf6f7a4 — sub_F6F7A4
+#[doc(alias = "sub_F6F7A4")]
+pub fn stub_f6f7a4() -> ! {
+    todo!("0xf6f7a4 sub_F6F7A4")
+}
+
+// 0xf6f7b0 — sub_F6F7B0
+#[doc(alias = "sub_F6F7B0")]
+pub fn stub_f6f7b0() -> ! {
+    todo!("0xf6f7b0 sub_F6F7B0")
+}
+
+// 0xf6f7bc — sub_F6F7BC
+#[doc(alias = "sub_F6F7BC")]
+pub fn stub_f6f7bc() -> ! {
+    todo!("0xf6f7bc sub_F6F7BC")
+}
+
+// 0xf6f7c8 — sub_F6F7C8
+#[doc(alias = "sub_F6F7C8")]
+pub fn stub_f6f7c8() -> ! {
+    todo!("0xf6f7c8 sub_F6F7C8")
+}
+
+// 0xf6f7d4 — sub_F6F7D4
+#[doc(alias = "sub_F6F7D4")]
+pub fn stub_f6f7d4() -> ! {
+    todo!("0xf6f7d4 sub_F6F7D4")
+}
+
+// 0xf6f7e0 — sub_F6F7E0
+#[doc(alias = "sub_F6F7E0")]
+pub fn stub_f6f7e0() -> ! {
+    todo!("0xf6f7e0 sub_F6F7E0")
+}
+
+// 0xf6f7ec — sub_F6F7EC
+#[doc(alias = "sub_F6F7EC")]
+pub fn stub_f6f7ec() -> ! {
+    todo!("0xf6f7ec sub_F6F7EC")
+}
+
+// 0xf6f7f8 — sub_F6F7F8
+#[doc(alias = "sub_F6F7F8")]
+pub fn stub_f6f7f8() -> ! {
+    todo!("0xf6f7f8 sub_F6F7F8")
+}
+
+// 0xf6f804 — sub_F6F804
+#[doc(alias = "sub_F6F804")]
+pub fn stub_f6f804() -> ! {
+    todo!("0xf6f804 sub_F6F804")
+}
+
+// 0xf6f810 — sub_F6F810
+#[doc(alias = "sub_F6F810")]
+pub fn stub_f6f810() -> ! {
+    todo!("0xf6f810 sub_F6F810")
+}
+
+// 0xf6f81c — sub_F6F81C
+#[doc(alias = "sub_F6F81C")]
+pub fn stub_f6f81c() -> ! {
+    todo!("0xf6f81c sub_F6F81C")
+}
+
+// 0xf6f828 — sub_F6F828
+#[doc(alias = "sub_F6F828")]
+pub fn stub_f6f828() -> ! {
+    todo!("0xf6f828 sub_F6F828")
+}
+
+// 0xf6f834 — sub_F6F834
+#[doc(alias = "sub_F6F834")]
+pub fn stub_f6f834() -> ! {
+    todo!("0xf6f834 sub_F6F834")
+}
+
+// 0xf6f840 — sub_F6F840
+#[doc(alias = "sub_F6F840")]
+pub fn stub_f6f840() -> ! {
+    todo!("0xf6f840 sub_F6F840")
+}
+
+// 0xf6f84c — sub_F6F84C
+#[doc(alias = "sub_F6F84C")]
+pub fn stub_f6f84c() -> ! {
+    todo!("0xf6f84c sub_F6F84C")
+}
+
+// 0xf6f858 — sub_F6F858
+#[doc(alias = "sub_F6F858")]
+pub fn stub_f6f858() -> ! {
+    todo!("0xf6f858 sub_F6F858")
+}
+
+// 0xf6f864 — sub_F6F864
+#[doc(alias = "sub_F6F864")]
+pub fn stub_f6f864() -> ! {
+    todo!("0xf6f864 sub_F6F864")
+}
+
+// 0xf6f870 — sub_F6F870
+#[doc(alias = "sub_F6F870")]
+pub fn stub_f6f870() -> ! {
+    todo!("0xf6f870 sub_F6F870")
+}
+
+// 0xf6f87c — sub_F6F87C
+#[doc(alias = "sub_F6F87C")]
+pub fn stub_f6f87c() -> ! {
+    todo!("0xf6f87c sub_F6F87C")
+}
+
+// 0xf6f888 — sub_F6F888
+#[doc(alias = "sub_F6F888")]
+pub fn stub_f6f888() -> ! {
+    todo!("0xf6f888 sub_F6F888")
+}
+
+// 0xf6f894 — sub_F6F894
+#[doc(alias = "sub_F6F894")]
+pub fn stub_f6f894() -> ! {
+    todo!("0xf6f894 sub_F6F894")
+}
+
+// 0xf6f8a0 — sub_F6F8A0
+#[doc(alias = "sub_F6F8A0")]
+pub fn stub_f6f8a0() -> ! {
+    todo!("0xf6f8a0 sub_F6F8A0")
+}
+
+// 0xf6f8ac — sub_F6F8AC
+#[doc(alias = "sub_F6F8AC")]
+pub fn stub_f6f8ac() -> ! {
+    todo!("0xf6f8ac sub_F6F8AC")
+}
+
+// 0xf6f8b8 — sub_F6F8B8
+#[doc(alias = "sub_F6F8B8")]
+pub fn stub_f6f8b8() -> ! {
+    todo!("0xf6f8b8 sub_F6F8B8")
+}
+
+// 0xf6f8c4 — sub_F6F8C4
+#[doc(alias = "sub_F6F8C4")]
+pub fn stub_f6f8c4() -> ! {
+    todo!("0xf6f8c4 sub_F6F8C4")
+}
+
+// 0xf6f8d0 — sub_F6F8D0
+#[doc(alias = "sub_F6F8D0")]
+pub fn stub_f6f8d0() -> ! {
+    todo!("0xf6f8d0 sub_F6F8D0")
+}
+
+// 0xf6f8dc — sub_F6F8DC
+#[doc(alias = "sub_F6F8DC")]
+pub fn stub_f6f8dc() -> ! {
+    todo!("0xf6f8dc sub_F6F8DC")
+}
+
+// 0xf6f8e8 — sub_F6F8E8
+#[doc(alias = "sub_F6F8E8")]
+pub fn stub_f6f8e8() -> ! {
+    todo!("0xf6f8e8 sub_F6F8E8")
+}
+
+// 0xf6f8f4 — sub_F6F8F4
+#[doc(alias = "sub_F6F8F4")]
+pub fn stub_f6f8f4() -> ! {
+    todo!("0xf6f8f4 sub_F6F8F4")
+}
+
+// 0xf6f900 — sub_F6F900
+#[doc(alias = "sub_F6F900")]
+pub fn stub_f6f900() -> ! {
+    todo!("0xf6f900 sub_F6F900")
+}
+
+// 0xf6f90c — sub_F6F90C
+#[doc(alias = "sub_F6F90C")]
+pub fn stub_f6f90c() -> ! {
+    todo!("0xf6f90c sub_F6F90C")
+}
+
+// 0xf6f918 — sub_F6F918
+#[doc(alias = "sub_F6F918")]
+pub fn stub_f6f918() -> ! {
+    todo!("0xf6f918 sub_F6F918")
+}
+
+// 0xf6f924 — sub_F6F924
+#[doc(alias = "sub_F6F924")]
+pub fn stub_f6f924() -> ! {
+    todo!("0xf6f924 sub_F6F924")
+}
+
+// 0xf6f930 — sub_F6F930
+#[doc(alias = "sub_F6F930")]
+pub fn stub_f6f930() -> ! {
+    todo!("0xf6f930 sub_F6F930")
+}
+
+// 0xf6f93c — sub_F6F93C
+#[doc(alias = "sub_F6F93C")]
+pub fn stub_f6f93c() -> ! {
+    todo!("0xf6f93c sub_F6F93C")
+}
+
+// 0xf6f948 — sub_F6F948
+#[doc(alias = "sub_F6F948")]
+pub fn stub_f6f948() -> ! {
+    todo!("0xf6f948 sub_F6F948")
+}
+
+// 0xf6f954 — sub_F6F954
+#[doc(alias = "sub_F6F954")]
+pub fn stub_f6f954() -> ! {
+    todo!("0xf6f954 sub_F6F954")
+}
+
+// 0xf6f960 — sub_F6F960
+#[doc(alias = "sub_F6F960")]
+pub fn stub_f6f960() -> ! {
+    todo!("0xf6f960 sub_F6F960")
+}
+
+// 0xf6f96c — sub_F6F96C
+#[doc(alias = "sub_F6F96C")]
+pub fn stub_f6f96c() -> ! {
+    todo!("0xf6f96c sub_F6F96C")
+}
