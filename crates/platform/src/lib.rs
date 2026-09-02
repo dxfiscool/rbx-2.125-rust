e7e34026bb263bac7098540e8193dcd96008aa66
dxf <dxf@local>
2026-09-02 16:43:17 +0200

    platform: add generated_plat_wd_watchdog15 100 stubs EA-sorted asc [skeleton batch watchdog15]

--- crates/platform/src/lib.rs
+++ crates/platform/src/lib.rs
@@ -364,3 +364,4 @@ pub mod generated_plat_wd_watchdog11;
 pub mod generated_plat_wd_watchdog12;
 pub mod generated_plat_wd_watchdog13;
 pub mod generated_plat_wd_watchdog14;
+pub mod generated_plat_wd_watchdog15;
