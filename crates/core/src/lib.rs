2c2043fed1597069d87e8a17c0da5bb63498c8a6
dxf <dxf@local>
2026-09-02 15:29:00 +0200

    core: add generated_core_wd_watchdog13 120 stubs [skeleton batch watchdog13]

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -502,4 +502,5 @@ pub mod generated_core_wd_watchdog10;
 pub mod generated_core_shard_od;
 pub mod generated_core_shard_oc;
 pub mod generated_core_wd_watchdog11;
+pub mod generated_core_wd_watchdog13;
 pub mod generated_gap_wd_watchdog;
