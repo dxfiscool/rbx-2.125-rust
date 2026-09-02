d79b03ffabaae3fb72a8da67de901e4fcfa22deb
dxf <dxf@local>
2026-09-02 23:23:29 +0200

    reflection: wire watchdog reflB

--- crates/reflection/src/lib.rs
+++ crates/reflection/src/lib.rs
@@ -327,3 +327,4 @@ pub mod generated_refl_wd_watchdog_a;
 pub mod generated_watchdog_refl_1788382772;
 pub mod generated_watchdog_refl_1788383407;
 pub mod generated_watchdog_reflA_1788384000;
+pub mod generated_watchdog_reflB_1788384000;
