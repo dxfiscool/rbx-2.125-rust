f4251e2089a073bd5593f9933f329a7b1447ab99
dxf <dxf@local>
2026-09-03 03:53:19 +0200

    watchdog script w4: 120 stubs

--- crates/script/src/lib.rs
+++ crates/script/src/lib.rs
@@ -273,6 +273,7 @@ pub mod generated_239;
 pub mod generated_240;
 pub mod generated_watchdog_scr_w1;
 pub mod generated_watchdog_script_w3;
+pub mod generated_watchdog_script_w4;
 pub mod generated_241;
 pub mod generated_242;
 pub mod generated_243;
