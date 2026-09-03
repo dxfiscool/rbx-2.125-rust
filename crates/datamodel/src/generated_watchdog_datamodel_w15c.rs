fcf24b10c122e8bd7d94f4b44cde4bb95d16463c
dxf <dxf@local>
2026-09-03 07:10:33 +0200

    datamodel: watchdog w15c sanitize alias

--- crates/datamodel/src/generated_watchdog_datamodel_w15c.rs
+++ crates/datamodel/src/generated_watchdog_datamodel_w15c.rs
@@ -82,7 +82,7 @@ pub fn stub_0xd2a8b4() -> ! {
 // 0xd2a8b8 -- __ZThn12_NK4Ogre14OverlayElement11getMaterialEv
 // `non-virtual thunk to'Ogre::OverlayElement::getMaterial(void)const
 // type: _DWORD __fastcall(Ogre::OverlayElement *__hidden this)
-#[doc(alias = "`non-virtual thunk to'Ogre::OverlayElement::getMaterial(void)const")]
+#[doc(alias = "`non-virtual thunk to Ogre::OverlayElement::getMaterial(void)const")]
 #[doc(alias = "__ZThn12_NK4Ogre14OverlayElement11getMaterialEv")]
 pub fn stub_0xd2a8b8() -> ! {
     todo!("0xd2a8b8 `non-virtual thunk to'Ogre::OverlayElement::getMaterial(void)const")
