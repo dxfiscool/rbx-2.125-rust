a8f350b6aef159b7b275525d8788872c1426f3ad
dxf <dxf@local>
2026-09-03 07:58:24 +0200

    reflection: watchdog w15d2 120 stubs

--- crates/reflection/src/lib.rs
+++ crates/reflection/src/lib.rs
@@ -370,3 +370,4 @@ pub mod generated_watchdog_reflection_w12b;
 pub mod generated_watchdog_reflection_w15;
 pub mod generated_watchdog_reflection_w15b;
 pub mod generated_watchdog_reflection_w15d;
+pub mod generated_watchdog_reflection_w15d2;
